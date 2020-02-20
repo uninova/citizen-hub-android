09109cde109d6235fc78ab6afaa6d5a3f1ab6d2f
Carlos Lopes <csl@uninova.pt>
2020-02-20 14:31:27 +0000

    Added MainActivity.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package pt.uninova.s4h.citizenhub.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import pt.uninova.s4h.citizenhub.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
