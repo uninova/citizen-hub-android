4bfd45d9e17130c21b202bb90e0c00c9c19769aa
jam <jam@uninova.pt>
2020-05-14 19:48:46 +0100

    Added base storage implementation.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/storage/Database.java
@@ -0,0 +1,76 @@
+package pt.uninova.s4h.citizenhub.storage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+
+    private static final int DATABASE_VERSION = 9;
+    SQLiteDatabase db;
+    private MeasurementsTable measurementsTable;
+    private SourceTable sourceTable;
+    private DeviceTable deviceTable;
+
+    /**
+     * Constructs a new instance of {@link DeviceTable}.
+     *
+     * @param context of the app
+     */
+    public Database(Context context, String filePath) {
+        super(context, filePath, null, DATABASE_VERSION);
+        db = this.getWritableDatabase();
+        deviceTable = new DeviceTable(db);
+        measurementsTable = new MeasurementsTable(db);
+        sourceTable = new SourceTable(db);
+    }
+//TODO   nullpointer exception
+// db.getDeviceTable().addRecord(name,address,state,type);
+
+    public void createTables(SQLiteDatabase db) {
+        DeviceTable.initialize(db);
+        MeasurementsTable.initialize(db);
+        SourceTable.initialize(db);
+    }
+
+    public DeviceTable getDeviceTable() {
+        return this.deviceTable;
+    }
+
+    public MeasurementsTable getMeasurementsTable() {
+        return this.measurementsTable;
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Execute the SQL statement
+        createTables(db);
+
+    }
+
+    public SourceTable getSourceTable() {
+        return this.sourceTable;
+    }
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        upgradeTables(db);
+    }
+
+
+    public void upgradeTables(SQLiteDatabase db) {
+        DeviceTable.upgrade(db);
+        MeasurementsTable.upgrade(db);
+        SourceTable.upgrade(db);
+        onCreate(db);
+    }
+
+
+}
---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/storage/DeviceColumns.java
@@ -0,0 +1,36 @@
+package pt.uninova.s4h.citizenhub.storage;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class DeviceColumns {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public static final String CONTENT_AUTHORITY = "pt.uninova.s4h.citizenhub";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_DEVICES = "devices";
+
+    private DeviceColumns() {
+    }
+
+    public static abstract class DeviceEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_DEVICES);
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DEVICES;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DEVICES;
+
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String TABLE_NAME = "devices";
+        public final static String COLUMN_DEVICE_NAME = "name";
+        public final static String COLUMN_DEVICE_ADDRESS = "address";
+        public final static String COLUMN_DEVICE_TYPE = "type";
+        public final static String COLUMN_DEVICE_STATE = "state";
+
+    }
+
+}
