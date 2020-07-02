1e654004f155109773673970c97c8b89d20dc748
jam <jam@uninova.pt>
2020-07-02 19:44:15 +0100

    added Source Database declaration to DatabaseManager

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage2/DatabaseManager.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage2/DatabaseManager.java
@@ -3,9 +3,11 @@ package pt.uninova.s4h.citizenhub.datastorage2;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Device.class}, version = 1)
+@Database(entities = {Device.class, Source.class}, version = 1)
 public abstract class DatabaseManager extends RoomDatabase {
 
     public abstract DeviceDAO deviceDao();
+
+    public abstract SourceDAO sourceDAO();
 
 }
