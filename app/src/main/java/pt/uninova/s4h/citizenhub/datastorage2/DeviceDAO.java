69997fffe5476c61330fa8a54d3b93f99e1e5fd6
jam <jam@uninova.pt>
2020-07-02 19:04:04 +0100

    Basic Cruid operations for deviceTable

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage2/DeviceDAO.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage2/DeviceDAO.java
@@ -1,9 +1,21 @@
 package pt.uninova.s4h.citizenhub.datastorage2;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
 
 @Dao
 public interface DeviceDAO {
 
-    
+    @Insert
+    String addDevice(Device device);
+
+    @Update
+    void updateDevice(Device device);
+
+    @Delete
+    void deleteDevice(Device device);
+
+
 }
