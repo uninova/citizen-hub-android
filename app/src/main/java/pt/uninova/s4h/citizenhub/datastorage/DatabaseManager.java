8732a5c7298d3ed50b82aabeaf05b728efef366e
jam <jam@uninova.pt>
2020-07-03 14:45:55 +0100

    Added measurements table to DatabaseManager

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseManager.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseManager.java
@@ -3,11 +3,13 @@ package pt.uninova.s4h.citizenhub.datastorage;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Device.class, Source.class}, version = 1)
+@Database(entities = {Device.class, Source.class, Measurement.class}, version = 1)
 public abstract class DatabaseManager extends RoomDatabase {
 
     public abstract DeviceDAO deviceDao();
 
     public abstract SourceDAO sourceDAO();
+
+    public abstract MeasurementDAO measurementDAO();
 
 }
