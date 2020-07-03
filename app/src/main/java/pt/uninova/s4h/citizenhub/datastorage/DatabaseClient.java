b4f37bf3837952cec724004b14ca0be12485cb3d
jam <jam@uninova.pt>
2020-07-03 16:42:08 +0100

    Added support to choose databaseName + updated room dependencies

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseClient.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseClient.java
@@ -8,20 +8,17 @@ public class DatabaseClient {
 
     private static DatabaseClient dbInstance;
     private Context context;
-    //our app database object
     private DatabaseManager dbManager;
 
-    private DatabaseClient(Context context) {
+    private DatabaseClient(Context context, String databaseName) {
         this.context = context;
 
-        //creating the app database with Room database builder
-        // name is the name of the database
-        dbManager = Room.databaseBuilder(context, DatabaseManager.class, "citizenhubDatabase").build();
+        dbManager = Room.databaseBuilder(context, DatabaseManager.class, databaseName).build();
     }
 
     public static synchronized DatabaseClient getInstance(Context context) {
         if (dbInstance == null) {
-            dbInstance = new DatabaseClient(context);
+            dbInstance = new DatabaseClient(context, "citizenhub.db");
         }
         return dbInstance;
     }
