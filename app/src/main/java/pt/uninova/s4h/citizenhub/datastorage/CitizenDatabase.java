7762464e16264fe0462244cda6d19676bf6771ce
jam <jam@uninova.pt>
2020-07-03 17:19:30 +0100

    Renamed class

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/CitizenDatabase.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/CitizenDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Device.class, Source.class, Measurement.class}, version = 1)
-public abstract class DatabaseManager extends RoomDatabase {
+public abstract class CitizenDatabase extends RoomDatabase {
 
     public abstract DeviceDAO deviceDao();
 
