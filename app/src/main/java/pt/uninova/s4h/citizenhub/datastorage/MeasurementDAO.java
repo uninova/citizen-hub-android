9dee8a388d361658133d1759aa48ef9a632a4d04
jam <jam@uninova.pt>
2020-07-03 15:22:53 +0100

    Replaced columnName to characteristicName and added getMeasurements() for a specified characteristic

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/MeasurementDAO.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/MeasurementDAO.java
@@ -27,17 +27,20 @@ public interface MeasurementDAO {
     @Query("select * from measurements")
     List<Measurement> getMeasurements();
 
-    @Query("SELECT AVG(value) FROM measurements WHERE name = :columnName AND timestamp =:date ")
-    int getAvgFromDay(String columnName, String date);
+    @Query("SELECT * FROM measurements WHERE name = :characteristicName")
+    List<Device> getMeasurementsWithCharacteristic(String characteristicName);
 
-    @Query("SELECT SUM(value) FROM measurements WHERE name = :columnName AND timestamp=:date ")
-    int getSumFromDay(String columnName, String date);
+    @Query("SELECT AVG(value) FROM measurements WHERE name = :characteristicName AND timestamp =:date ")
+    int getAvgFromDay(String characteristicName, String date);
 
-    @Query("SELECT AVG(value) FROM measurements WHERE name = :columnName  AND timestamp BETWEEN  :timestamp_start AND :timestamp_end")
-    int getAvgFromDayInterval(String columnName, String timestamp_start, String timestamp_end);
+    @Query("SELECT SUM(value) FROM measurements WHERE name = :characteristicName AND timestamp=:date ")
+    int getSumFromDay(String characteristicName, String date);
 
-    @Query("SELECT SUM(value) FROM measurements WHERE name = :columnName  AND timestamp BETWEEN  :timestamp_start AND :timestamp_end")
-    int getSumFromDayInterval(String columnName, String timestamp_start, String timestamp_end);
+    @Query("SELECT AVG(value) FROM measurements WHERE name = :characteristicName  AND timestamp BETWEEN  :timestamp_start AND :timestamp_end")
+    int getAvgFromDayInterval(String characteristicName, String timestamp_start, String timestamp_end);
+
+    @Query("SELECT SUM(value) FROM measurements WHERE name = :characteristicName  AND timestamp BETWEEN  :timestamp_start AND :timestamp_end")
+    int getSumFromDayInterval(String characteristicName, String timestamp_start, String timestamp_end);
 
 
 }
