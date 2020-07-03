9e710dfba72770b0ba4f8439d60dc86dbaa4d65d
jam <jam@uninova.pt>
2020-07-03 14:34:03 +0100

    Added Measurements table, constructor and getters&setters

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/Measurement.java
@@ -0,0 +1,66 @@
+package pt.uninova.s4h.citizenhub.datastorage;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "measurements")
+public class Measurement {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ForeignKey(entity = Source.class, parentColumns = "uuid", childColumns = "uuid", onDelete = CASCADE)
+    private String uuid;
+    private String timestamp;
+    private String value;
+    private String name;
+
+    @Ignore
+    public Measurement() {
+
+    }
+
+
+    public Measurement(Integer id, String uuid, String timestamp, String value, String name) {
+        this.id = id;
+        this.uuid = uuid;
+        this.timestamp = timestamp;
+        this.value = value;
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
