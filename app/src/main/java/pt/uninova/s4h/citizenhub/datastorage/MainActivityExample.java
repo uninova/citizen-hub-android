ad72a94d90bedad17f5d6b9b922e89daf00af57e
jam <jam@uninova.pt>
2020-07-15 17:57:56 +0100

    Example code of ViewModel for activities - to be deleted in future

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/MainActivityExample.java
@@ -0,0 +1,31 @@
+package pt.uninova.s4h.citizenhub.datastorage;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
+
+import pt.uninova.s4h.citizenhub.ui.R;
+
+public class MainActivityExample extends AppCompatActivity {
+    private DeviceViewModel deviceViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        deviceViewModel = ViewModelProviders.of(this).get(DeviceViewModel.class);
+        deviceViewModel.getAllDevices().observe(this, new Observer<List<Device>>() {
+            @Override
+            public void onChanged(@Nullable List<Device> devices) {
+                //update RecyclerView
+                Toast.makeText(getApplicationContext(), "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
