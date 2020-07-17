7498fa810083f8f452973a8f40ec5ca962ccdcac
jam <jam@uninova.pt>
2020-07-17 20:13:25 +0100

    Added Converters Class to handle Dates to and from Database

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/CitizenDatabase.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/CitizenDatabase.java
@@ -2,8 +2,11 @@ package pt.uninova.s4h.citizenhub.datastorage;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {Device.class, Source.class, Measurement.class}, version = 1)
+@TypeConverters({Converters.class})
+
 public abstract class CitizenDatabase extends RoomDatabase {
 
     public abstract DeviceDAO deviceDao();
---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/Converters.java
@@ -0,0 +1,17 @@
+package pt.uninova.s4h.citizenhub.datastorage;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class Converters {
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+}
