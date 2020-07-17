7d93b2c382b912d0cd13ef0c5dd8e4979a26691b
jam <jam@uninova.pt>
2020-07-17 20:34:48 +0100

    Added SumMeasurement DatabaseView

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/SumMeasurement.java
@@ -0,0 +1,12 @@
+package pt.uninova.s4h.citizenhub.datastorage;
+
+import androidx.room.DatabaseView;
+
+import java.util.Date;
+
+@DatabaseView("SELECT SUM(value) AS averageValue FROM measurements WHERE timestamp BETWEEN date() AND date()")
+public class SumMeasurement {
+    public String averageValue;
+    public Date from;
+    public Date to;
+}
