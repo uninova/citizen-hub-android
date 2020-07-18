5e341b71d8edf10e59fdebae3572ba70a87568c8
jam <jam@uninova.pt>
2020-07-18 13:38:51 +0100

    DatabaseView folder with specific views

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseViews/HeartRateView.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/DatabaseViews/HeartRateView.java
@@ -1,11 +1,11 @@
-package pt.uninova.s4h.citizenhub.datastorage;
+package pt.uninova.s4h.citizenhub.datastorage.DatabaseViews;
 
 import androidx.room.DatabaseView;
 
 import java.util.Date;
 
 @DatabaseView("SELECT value, timestamp FROM measurements WHERE name = 'HeartRate' ORDER BY timestamp ASC")
-public class AvgMeasurementView {
+public class HeartRateView {
     public float value;
     public Date timestamp;
 }
