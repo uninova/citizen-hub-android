b2afc02c158f39f272763cf87eab3a9c5427b565
jam <jam@uninova.pt>
2020-07-21 11:12:08 +0100

    Converter fixed for Date -> String and String -> Date

--- app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/Converters.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/datastorage/Converters.java
@@ -2,16 +2,26 @@ package pt.uninova.s4h.citizenhub.datastorage;
 
 import androidx.room.TypeConverter;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Converters {
+
+    private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value);
+    public static Date fromTimestamp(String value) {
+        try {
+            return value == null ? null : df.parse(value);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
+    public static String dateToTimestamp(Date date) {
+        return date == null ? null : df.format(date);
     }
 }
