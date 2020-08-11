c5b8718620e1b488d2cef46965a52db544179f4d
Carlos Lopes <csl@uninova.pt>
2020-08-11 18:04:59 +0100

    Added new navigation model.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -0,0 +1,45 @@
+package pt.uninova.s4h.citizenhub;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.nav_view);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.summary_fragment, R.id.report_fragment, R.id.user_fragment, R.id.device_list_fragment, R.id.about_fragment)
+                        .setOpenableLayout(drawerLayout)
+                        .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
