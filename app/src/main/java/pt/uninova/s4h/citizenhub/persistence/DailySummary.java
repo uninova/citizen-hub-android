7c603f3b9b30dfa65884d3cc13e7a77cb5cb36dc
Carlos Lopes <csl@uninova.pt>
2020-08-21 17:49:17 +0100

    Added DailySummary access process.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/DailySummary.java
@@ -0,0 +1,82 @@
+package pt.uninova.s4h.citizenhub.persistence;
+
+import androidx.room.ColumnInfo;
+import androidx.room.DatabaseView;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
+@DatabaseView(value = "SELECT DISTINCT date, (SELECT AVG(value) FROM date_measurement WHERE date_measurement.kind_id = 1 AND date_measurement.date = main.date GROUP BY date) average_heart_rate, (SELECT SUM(value) FROM date_measurement WHERE date_measurement.kind_id = 2 AND date_measurement.date = main.date GROUP BY date) sum_steps, (SELECT SUM(value) FROM date_measurement WHERE date_measurement.kind_id = 3 AND date_measurement.date = main.date GROUP BY date) sum_distance, (SELECT SUM(value) FROM date_measurement WHERE date_measurement.kind_id = 4 AND date_measurement.date = main.date GROUP BY date) sum_calories, (SELECT SUM(value) FROM date_measurement WHERE date_measurement.kind_id = 5 AND date_measurement.date = main.date GROUP BY date) sum_good_posture, (SELECT SUM(value) FROM date_measurement WHERE date_measurement.kind_id = 6 AND date_measurement.date = main.date GROUP BY date) sum_bad_posture FROM date_measurement AS main", viewName = "daily_summary")
+public class DailySummary {
+
+    @ColumnInfo(name = "average_heart_rate")
+    private float averageHeartRate;
+    @TypeConverters(TimestampConverter.class)
+    private Date date;
+    @ColumnInfo(name = "sum_bad_posture")
+    private float sumBadPosture;
+    @ColumnInfo(name = "sum_calories")
+    private int sumCalories;
+    @ColumnInfo(name = "sum_distance")
+    private int sumDistance;
+    @ColumnInfo(name = "sum_good_posture")
+    private float sumGoodPosture;
+    @ColumnInfo(name = "sum_steps")
+    private int sumSteps;
+
+    public float getAverageHeartRate() {
+        return averageHeartRate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public float getSumBadPosture() {
+        return sumBadPosture;
+    }
+
+    public int getSumCalories() {
+        return sumCalories;
+    }
+
+    public int getSumDistance() {
+        return sumDistance;
+    }
+
+    public float getSumGoodPosture() {
+        return sumGoodPosture;
+    }
+
+    public int getSumSteps() {
+        return sumSteps;
+    }
+
+    public void setAverageHeartRate(float averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setSumBadPosture(float sumBadPosture) {
+        this.sumBadPosture = sumBadPosture;
+    }
+
+    public void setSumCalories(int sumCalories) {
+        this.sumCalories = sumCalories;
+    }
+
+    public void setSumDistance(int sumDistance) {
+        this.sumDistance = sumDistance;
+    }
+
+    public void setSumGoodPosture(float sumGoodPosture) {
+        this.sumGoodPosture = sumGoodPosture;
+    }
+
+    public void setSumSteps(int sumSteps) {
+        this.sumSteps = sumSteps;
+    }
+}
