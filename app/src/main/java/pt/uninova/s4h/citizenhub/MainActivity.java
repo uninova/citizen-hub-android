2b037c0616a2f9b7393153462702c2150ee9aa80
Carlos Lopes <csl@uninova.pt>
2020-08-21 17:53:05 +0100

    Added random record insertion in database for testing.

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -1,6 +1,7 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -9,6 +10,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
+import pt.uninova.s4h.citizenhub.persistence.Measurement;
+import pt.uninova.s4h.citizenhub.persistence.MeasurementRepository;
+
+import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +42,33 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //
+        // SAMPLING CODE
+        //
+        Handler handler = new Handler();
+        int delay = 500;
+        final Random random = new Random();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Measurement measurement = new Measurement();
+                measurement.setTimestamp(new Date());
+                measurement.setType(1);
+                measurement.setValue("" + random.nextInt(200));
+
+                MeasurementRepository repo = new MeasurementRepository(getApplication());
+                repo.insert(measurement);
+
+                handler.postDelayed(this, delay);
+                System.out.println("DO SOMETHING!");
+            }
+        }, delay);
+        //
+        // END SAMPLING CODE
+        //
     }
 
     @Override
