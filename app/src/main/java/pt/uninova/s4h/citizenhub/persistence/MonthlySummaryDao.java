9c541ef4ae10a1f6175eb581b6f7018631e69384
jam <jam@uninova.pt>
2020-08-23 11:08:18 +0100

    Added query to retrieve livedata list of days in a month in a year

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MonthlySummaryDao.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MonthlySummaryDao.java
@@ -12,4 +12,9 @@ import java.util.List;
 public interface MonthlySummaryDao {
     @Query("SELECT CAST(strftime('%d', datetime(date, 'unixepoch')) AS int) AS date from date_measurement WHERE date BETWEEN catchTheFirstDayOfTheMonth(:month) AND catchTheLastDayOfTheMonth(:month)")
     LiveData<List<Integer>> getDaysWithSummary(Integer month);
+
+    @Query("select strftime('%d',datetime(date, 'unixepoch')) from date_measurement \n" +
+            "where strftime('%m',datetime(date, 'unixepoch')) = :month \n" +
+            "and strftime('%Y',datetime(date, 'unixepoch')) = :year;")
+    LiveData<List<Integer>> getDaysWithDataInMonthYear(Integer month, Integer year);
 }
