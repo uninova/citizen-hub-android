d998fc2e6b6acfce023c4446713ec555c5bbd0da
jam <jam@uninova.pt>
2020-08-24 18:27:36 +0100

    Added ReportMasterViewModel for monthly reports

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/ReportMasterViewModel.java
@@ -0,0 +1,25 @@
+package pt.uninova.s4h.citizenhub.persistence;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ReportMasterViewModel extends AndroidViewModel {
+
+    private final ReportMasterRepository reportMasterRepository;
+    private final LiveData<List<Integer>> reportMasterSummary;
+
+    public ReportMasterViewModel(Application application, int year, int month) {
+        super(application);
+
+        reportMasterRepository = new ReportMasterRepository(application, year, month);
+        reportMasterSummary = reportMasterRepository.getReportMasterSummary();
+    }
+
+    public LiveData<List<Integer>> getReportMasterSummary() {
+        return reportMasterSummary;
+    }
+}
