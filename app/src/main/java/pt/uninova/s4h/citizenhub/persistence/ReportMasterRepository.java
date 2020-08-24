62614229691dca41a7664ff4b61cb0f85556ab96
jam <jam@uninova.pt>
2020-08-24 18:27:21 +0100

    Added ReportMasterRepository for monthly reports

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/ReportMasterRepository.java
@@ -0,0 +1,25 @@
+package pt.uninova.s4h.citizenhub.persistence;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ReportMasterRepository {
+
+    private final DailySummaryDao dailySummaryDao;
+    private final LiveData<List<Integer>> reportMasterSummary;
+
+    public ReportMasterRepository(Application application, int year, int month) {
+        CitizenHubDatabase citizenHubDatabase = CitizenHubDatabase.getInstance(application);
+
+        dailySummaryDao = citizenHubDatabase.dailySummaryDao();
+
+        reportMasterSummary = dailySummaryDao.getDaysWithDataInMonthYear( year, month );
+    }
+
+    public LiveData<List<Integer>> getReportMasterSummary() {
+        return reportMasterSummary;
+    }
+}
