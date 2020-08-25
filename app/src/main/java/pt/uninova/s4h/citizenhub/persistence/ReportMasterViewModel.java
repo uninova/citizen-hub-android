487fc9eae1c71c5dc2545db419dbc72eaefaaac0
jam <jam@uninova.pt>
2020-08-25 13:20:02 +0100

    Applied same structure as other ViewModel's

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/ReportMasterViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/ReportMasterViewModel.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 public class ReportMasterViewModel extends AndroidViewModel {
 
-    private final ReportMasterRepository reportMasterRepository;
+    private final LiveData<List<Integer>> reportMasterSummary;
 
-    public ReportMasterViewModel(Application application) {
+    public ReportMasterViewModel(Application application, int year, int month) {
         super(application);
-
-        reportMasterRepository = new ReportMasterRepository(application);
+        final ReportMasterRepository reportMasterRepository = new ReportMasterRepository(application);
+        reportMasterSummary = reportMasterRepository.getReportMasterSummary(year,month);
     }
 
-    public LiveData<List<Integer>> getReportMasterSummary(int year, int month) {
-        return reportMasterRepository.getReportMasterSummary(year, month);
+    public LiveData<List<Integer>> getReportMasterSummary() {
+        return reportMasterSummary;
     }
 }
