bdf4a96c76cc30c2a2f0832b48653370d3bda1bf
Carlos Lopes <csl@uninova.pt>
2020-08-26 18:41:52 +0100

    Modified to naming convention.

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/TimestampConverter.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/TimestampConverter.java
@@ -7,12 +7,13 @@ import java.util.Date;
 public class TimestampConverter {
 
     @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value * 1000L);
+    public static Long fromDate(Date date) {
+        return date == null ? null : date.getTime() / 1000L;
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime() / 1000L;
+    public static Date toDate(Long value) {
+        return value == null ? null : new Date(value * 1000L);
     }
+
 }
