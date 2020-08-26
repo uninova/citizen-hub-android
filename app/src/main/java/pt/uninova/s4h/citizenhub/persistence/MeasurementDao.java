d6c47e9e4c27940c347acdc7cfde4dee2c0fce16
jam <jam@uninova.pt>
2020-08-26 15:44:47 +0100

    Removed queries that won't be used

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MeasurementDao.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MeasurementDao.java
@@ -15,15 +15,6 @@ public interface MeasurementDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void addMeasurement(Measurement measurement);
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    void updateMeasurement(Measurement measurement);
-
-    @Delete
-    void deleteMeasurement(Measurement measurement);
-
-    @Query("DELETE FROM measurement")
-    void deleteAllMeasurements();
-
     @Query("SELECT * FROM measurement")
     List<Measurement> getMeasurements();
 
