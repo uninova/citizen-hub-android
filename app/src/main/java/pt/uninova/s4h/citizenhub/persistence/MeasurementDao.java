198c4ff84f43fd4353b5f535c9babed2e8126558
Carlos Lopes <csl@uninova.pt>
2020-08-26 01:53:55 +0100

    Organized imports.

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MeasurementDao.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/MeasurementDao.java
@@ -1,13 +1,7 @@
 package pt.uninova.s4h.citizenhub.persistence;
 
 import androidx.lifecycle.LiveData;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-import androidx.room.TypeConverters;
-import androidx.room.Update;
+import androidx.room.*;
 
 import java.util.Date;
 import java.util.List;
