9eeb2222f8e4aaf874f928f56ee6e9a84c36015d
jam <jam@uninova.pt>
2020-09-02 16:46:54 +0100

    Added "Application" variable.

--- app/src/main/java/pt/uninova/s4h/citizenhub/DeviceListFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceListFragment.java
@@ -1,9 +1,13 @@
 package pt.uninova.s4h.citizenhub;
 
+import android.app.Application;
+import android.bluetooth.le.ScanResult;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -12,6 +16,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Objects;
+
+import pt.uninova.s4h.citizenhub.persistence.Device;
+import pt.uninova.s4h.citizenhub.persistence.DeviceRepository;
+import pt.uninova.s4h.citizenhub.service.CitizenHubServiceBound;
 
 public class DeviceListFragment extends Fragment {
 
@@ -19,11 +28,19 @@ public class DeviceListFragment extends Fragment {
     private DeviceListAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<DeviceListItem> deviceList;
+    private Application app;
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_device_list, menu);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (Application) requireActivity().getApplication();
+
     }
 
     @Override
@@ -40,7 +57,16 @@ public class DeviceListFragment extends Fragment {
 
     private void createList(){
         deviceList = new ArrayList<>(); //TODO remove this, testing
+        DeviceRepository repo = new DeviceRepository(app);
+
+//        if(repo.getAllDevices().getValue() !=null) {
+//            for (Device d : (repo.getAllDevices().getValue())) {
+//                Log.d("Devices", d.getName() + " " + d.getAddress() + " " + d.getState() + " " + d.getType());
+//            }
+//        }
         for(int i=0; i<10; i++) {
+
+
             deviceList.add(new DeviceListItem(R.drawable.ic_watch, "Watch1", "It's good.", R.drawable.ic_settings));
             deviceList.add(new DeviceListItem(R.drawable.ic_watch_off, "Watch2", "It's bad.", R.drawable.ic_settings_off));
             deviceList.add(new DeviceListItem(R.drawable.ic_watch, "Watch3", "It's meh.", R.drawable.ic_settings));
@@ -113,8 +139,20 @@ public class DeviceListFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_fragment_device_list_search) {
             Navigation.findNavController(getView()).navigate(DeviceListFragmentDirections.actionDeviceListFragmentToDeviceSearchFragment());
-        }
+//            Intent intent = new Intent(this, CitizenHubService.class);
+//
+           // getActivity().startService(new Intent(getActivity(),CitizenHubService.class));
+           CitizenHubServiceBound activity = (CitizenHubServiceBound) getActivity();
+            activity.getService().StartScan();
+            DeviceRepository repo = new DeviceRepository(app);
 
+            Device device1 = new Device("HexoTest", "39:42:45:69", "Health", "on");
+            Device device2 = new Device("ChineseHexo", "39:42:45:69", "Unknown", "off");
+            Device device3 = new Device("KBZ", "39:42:45:69", "Health", "on");
+            repo.insert(device1);
+            repo.insert(device2);
+            repo.insert(device3);
+        }
         return super.onOptionsItemSelected(item);
     }
 }
