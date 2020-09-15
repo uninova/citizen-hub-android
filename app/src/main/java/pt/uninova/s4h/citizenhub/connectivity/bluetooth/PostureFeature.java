3fa84fb70f51f2e7ee60d062ddfb23c60b46e727
jam <jam@uninova.pt>
2020-09-15 15:50:20 +0100

    PostureFeature boilerplate code.

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/PostureFeature.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/PostureFeature.java
@@ -1,4 +1,46 @@
 package pt.uninova.s4h.citizenhub.connectivity.bluetooth;
 
-public class PostureFeature {
-}
+import java.util.UUID;
+
+import pt.uninova.s4h.citizenhub.service.DeviceManager;
+
+public class PostureFeature extends BluetoothFeature {
+
+    private static final UUID UUID_HEART_RATE_MEASUREMENT = UUID.fromString("");
+    private static UUID postureServiceUUID = UUID.fromString("");
+    private static UUID postureCharacteristicUUID = UUID.fromString("");
+    private static UUID postureDescriptorUUID;
+    private static UUID uuid = DeviceManager.namespaceGenerator().getUUID("bluetooth.generic.posture_measurement");
+
+public  PostureFeature(BluetoothConnection connection, UUID id) {
+        super(connection, id);
+        }
+
+
+@Override
+public UUID getId() {
+        return uuid;
+        }
+
+@Override
+public boolean enable() {
+        try {
+        getConnection().enableNotifications(postureServiceUUID, postureCharacteristicUUID);
+        // observer -> measurementRepository.add;
+        return true;
+        } catch (Exception e) {
+        return false;
+        }
+        }
+
+@Override
+public boolean disable() {
+        try {
+        getConnection().disableNotifications(postureServiceUUID, postureCharacteristicUUID);
+        return true;
+        } catch (Exception e) {
+        return false;
+        // measurementRepository.add;
+        }
+        }
+        }
