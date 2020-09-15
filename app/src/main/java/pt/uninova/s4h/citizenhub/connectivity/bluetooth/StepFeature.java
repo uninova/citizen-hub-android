29e51cd9425295e81f6acff2b02da1e51dafdd14
jam <jam@uninova.pt>
2020-09-15 15:50:28 +0100

    StepFeature boilerplate code.

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/StepFeature.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/StepFeature.java
@@ -1,4 +1,47 @@
 package pt.uninova.s4h.citizenhub.connectivity.bluetooth;
 
-public class StepFeature {
+import java.util.UUID;
+
+import pt.uninova.s4h.citizenhub.service.DeviceManager;
+
+public class StepFeature extends BluetoothFeature {
+
+    private static final UUID UUID_HEART_RATE_MEASUREMENT = UUID.fromString("");
+    private static UUID stepServiceUUID = UUID.fromString("");
+    private static UUID stepCharacteristicUUID = UUID.fromString("");
+    private static UUID stepDescriptorUUID;
+    private static UUID uuid = DeviceManager.namespaceGenerator().getUUID("bluetooth.generic.step_measurement");
+
+    public  StepFeature(BluetoothConnection connection, UUID id) {
+        super(connection, id);
+    }
+
+
+    @Override
+    public UUID getId() {
+        return uuid;
+    }
+
+    @Override
+    public boolean enable() {
+        try {
+            getConnection().enableNotifications(stepServiceUUID, stepCharacteristicUUID);
+            // observer -> measurementRepository.add;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean disable() {
+        try {
+            getConnection().disableNotifications(stepServiceUUID, stepCharacteristicUUID);
+            return true;
+        } catch (Exception e) {
+            return false;
+            // measurementRepository.add;
+        }
+    }
 }
+
