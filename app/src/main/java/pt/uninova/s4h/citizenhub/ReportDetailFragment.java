af4c5a19458dd05c8fd4cb0f19886114fad07559
Carlos Lopes <csl@uninova.pt>
2020-09-22 01:47:34 +0100

    Added PDF creation and upload

--- app/src/main/java/pt/uninova/s4h/citizenhub/ReportDetailFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ReportDetailFragment.java
@@ -1,27 +1,24 @@
 package pt.uninova.s4h.citizenhub;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import care.data4life.sdk.Data4LifeClient;
+import care.data4life.fhir.stu3.model.DocumentReference;
+import care.data4life.sdk.lang.D4LException;
+import care.data4life.sdk.listener.ResultListener;
+import care.data4life.sdk.model.Record;
 import org.jetbrains.annotations.NotNull;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementAggregate;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementKind;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.Map;
 
 public class ReportDetailFragment extends Fragment {
@@ -29,20 +26,27 @@ public class ReportDetailFragment extends Fragment {
     private ReportViewModel model;
     private TextView infoTextView_timeSitting, infoTextView_timePosture, infoTextView_distance, infoTextView_steps,
             infoTextView_calories, infoTextView_heartrate, infoTextView_day;
-    private Button uploadPdfButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_report_detail, container, false);
 
-        uploadPdfButton = view.findViewById(R.id.button);
+        Button uploadPdfButton = view.findViewById(R.id.button);
         uploadPdfButton.setOnClickListener(v -> {
-            Data4LifeClient client = Data4LifeClient.getInstance();
+            model.sendDetail(new ResultListener<Record<DocumentReference>>() {
+                @Override
+                public void onSuccess(Record<DocumentReference> record) {
+                    requireActivity().runOnUiThread(() -> Toast.makeText(getContext(), "File uploaded successfully.", Toast.LENGTH_SHORT).show());
+                }
 
-            DocumentReference documentReference = DocumentReferenceBuilder.buildWith(
-                    "Daily Report " + model.getDetailDate().toString(),
-                    );
-            client.createRecord();
+                @Override
+                public void onError(D4LException exception) {
+                    requireActivity().runOnUiThread(() -> {
+                        Toast.makeText(getContext(), "File failed to upload.", Toast.LENGTH_SHORT).show();
+                        exception.printStackTrace();
+                    });
+                }
+            });
         });
 
         return view;
--- app/src/main/java/pt/uninova/s4h/citizenhub/ReportViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ReportViewModel.java
@@ -1,22 +1,34 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.app.Application;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import care.data4life.fhir.stu3.model.*;
+import care.data4life.sdk.Data4LifeClient;
+import care.data4life.sdk.config.DataRestrictionException;
+import care.data4life.sdk.helpers.AttachmentBuilder;
+import care.data4life.sdk.helpers.DocumentReferenceBuilder;
+import care.data4life.sdk.helpers.PractitionerBuilder;
+import care.data4life.sdk.listener.ResultListener;
+import care.data4life.sdk.model.Record;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementAggregate;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementKind;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementRepository;
-import pt.uninova.util.messaging.Observer;
 import pt.uninova.util.Pair;
+import pt.uninova.util.messaging.Observer;
 import pt.uninova.util.time.LocalDateInterval;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class ReportViewModel extends AndroidViewModel {
 
@@ -28,6 +40,7 @@ public class ReportViewModel extends AndroidViewModel {
     final private Set<Pair<Integer, Integer>> peekedMonths;
 
     private LocalDate detailDate;
+    private Map<MeasurementKind, MeasurementAggregate> detailAggregates;
 
     public ReportViewModel(Application application) {
         super(application);
@@ -44,6 +57,77 @@ public class ReportViewModel extends AndroidViewModel {
         detailDate = LocalDate.now();
 
         peek();
+    }
+
+    static Practitioner getFakePractitioner() {
+        return PractitionerBuilder.buildWith(
+                "Bruce",
+                "Banner",
+                "Dr.",
+                "MD",
+                "Walvisbaai 3",
+                "2333ZA",
+                "Den helder",
+                "+31715269111",
+                "www.webpage.com");
+    }
+
+    static CodeableConcept getFakePracticeSpeciality() {
+        Coding coding = new Coding();
+        coding.code = "General Medicine";
+        coding.display = "General Medicine";
+        coding.system = "http://www.ihe.net/xds/connectathon/practiceSettingCodes";
+
+        CodeableConcept concept = new CodeableConcept();
+        concept.coding = Arrays.asList(coding);
+        return concept;
+    }
+
+    static CodeableConcept getFakeDocumentReferenceType() {
+        Coding coding = new Coding();
+        coding.code = "34108-1";
+        coding.display = "Outpatient Note";
+        coding.system = "http://loinc.org";
+
+        CodeableConcept concept = new CodeableConcept();
+        concept.coding = Arrays.asList(coding);
+        return concept;
+    }
+
+    public byte[] createPdf() {
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1200, 1000, 1).create();
+        Paint myPaint = new Paint();
+        Paint titlePaint = new Paint();
+        PdfDocument.Page page = document.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+
+        titlePaint.setTextAlign(Paint.Align.LEFT);
+        titlePaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titlePaint.setTextSize(30);
+
+        int y = 50;
+
+        for (MeasurementKind i : detailAggregates.keySet()) {
+            MeasurementAggregate agg = detailAggregates.get(i);
+
+            canvas.drawText(i.name() + " Sum: " + agg.getSum() + "; Avg: " + agg.getAverage() + "; Min: " + agg.getMin() + "; Max: " + agg.getMax() + "; Count: " + agg.getCount(), 200, y, titlePaint);
+            y += 50;
+        }
+
+        document.finishPage(page);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            document.writeTo(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        document.close();
+
+        return out.toByteArray();
     }
 
     private void onDateBoundsChanged(LocalDateInterval dateBounds) {
@@ -69,7 +153,11 @@ public class ReportViewModel extends AndroidViewModel {
     }
 
     public void obtainSummary(Observer<Map<MeasurementKind, MeasurementAggregate>> observer) {
-        repository.obtainDailyAggregate(detailDate, observer);
+        repository.obtainDailyAggregate(detailDate, value -> {
+            detailAggregates = value;
+
+            observer.onChanged(value);
+        });
     }
 
     private void onDatesChanged(List<LocalDate> dates) {
@@ -96,4 +184,44 @@ public class ReportViewModel extends AndroidViewModel {
             repository.obtainDates(peek, this::onDatesChanged);
         }
     }
+
+    public void sendDetail(ResultListener<Record<DocumentReference>> resultListener) {
+        Data4LifeClient client = Data4LifeClient.getInstance();
+        LocalDateTime now = LocalDateTime.now();
+        List<Attachment> attachments = new ArrayList<>(1);
+
+        byte[] data = createPdf();
+        Attachment attach = null;
+
+        try {
+            attach = AttachmentBuilder.buildWith(now.toString(),
+                    new FhirDateTime(new FhirDate(now.getYear(), now.getMonthValue(), now.getDayOfMonth()),
+                            new FhirTime(now.getHour(), now.getMinute(), now.getSecond(), null, null),
+                            TimeZone.getDefault()),
+                    "application/pdf",
+                    data);
+        } catch (DataRestrictionException.UnsupportedFileType | DataRestrictionException.MaxDataSizeViolation unsupportedFileType) {
+            unsupportedFileType.printStackTrace();
+        }
+
+        attachments.add(attach);
+
+        DocumentReference documentReference = DocumentReferenceBuilder.buildWith(
+                "Daily Report " + detailDate.toString(),
+                new FhirInstant(
+                        new FhirDateTime(
+                                new FhirDate(now.getYear(), now.getMonthValue(), now.getDayOfMonth()),
+                                new FhirTime(now.getHour(), now.getMinute(), now.getSecond(), null, null),
+                                TimeZone.getDefault())),
+                CodeSystems.DocumentReferenceStatus.CURRENT,
+                attachments,
+                getFakeDocumentReferenceType(),
+                getFakePractitioner(),
+                getFakePracticeSpeciality()
+        );
+
+        client.createRecord(documentReference, resultListener);
+    }
+
+
 }
