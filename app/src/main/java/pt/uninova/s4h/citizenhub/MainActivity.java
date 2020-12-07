c74d017049ad3a2ea6e39da68fd8d7417ac27a70
jam7k <jam@uninova.pt>
2020-12-07 15:54:40 +0000

    Added ability for the activity to send the results to their respective fragments.

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -96,6 +99,13 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
     @Override
     public CitizenHubService getService() {
