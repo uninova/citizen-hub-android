947e1c6ea087cf91ce8fdd8faa5d3553b069c981
jam7k <jam@uninova.pt>
2020-12-09 12:01:20 +0000

    Added is paired feature to viewmodel.

--- app/src/main/java/pt/uninova/s4h/citizenhub/DeviceViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceViewModel.java
@@ -7,12 +7,17 @@ import androidx.lifecycle.MutableLiveData;
 import pt.uninova.s4h.citizenhub.persistence.Device;
 import pt.uninova.s4h.citizenhub.persistence.DeviceRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DeviceViewModel extends AndroidViewModel {
     private MutableLiveData<Device> device;
     private LiveData<List<Device>> deviceList;
     final private DeviceRepository deviceRepository;
+    private List<Device> pairedDevices;
+    private HashSet<String> devices;
 
     public DeviceViewModel(Application application) {
         super(application);
@@ -23,6 +28,11 @@ public class DeviceViewModel extends AndroidViewModel {
 
     public LiveData<List<Device>> getDevices() {
         return deviceList;
+    }
+
+    public boolean isDevicePaired (String address){
+        pairedDevices = deviceList.getValue();
+        return pairedDevices.stream().anyMatch(item -> address.equals(item.getAddress()));
     }
 
     public MutableLiveData<Device> getSelectedDevice() {
