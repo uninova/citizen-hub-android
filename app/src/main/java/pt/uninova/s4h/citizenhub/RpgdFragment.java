2198e76f007a7d5937792966593bfc7fcf329d8b
jam7k <jam@uninova.pt>
2020-12-14 21:46:54 +0000

    Added boilerplate for RPGD fragment

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/RpgdFragment.java
@@ -0,0 +1,37 @@
+package pt.uninova.s4h.citizenhub;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import pt.uninova.s4h.citizenhub.R;
+
+public class RpgdFragment extends Fragment {
+
+    private TextView rpgdInformation;
+    private Application app;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View result = inflater.inflate(R.layout.fragment_device_add, container, false);
+        return result;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (Application) requireActivity().getApplication();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
