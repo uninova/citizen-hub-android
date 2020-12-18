5244acd788c0170af60a98afe475e4f057609ae8
jam7k <jam@uninova.pt>
2020-12-18 15:18:09 +0000

    Added OpenSourceLicensesFragment and navigation to it.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/OpenSourceLicensesFragment.java
@@ -0,0 +1,45 @@
+package pt.uninova.s4h.citizenhub;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OpenSourceLicensesFragment extends Fragment {
+
+    private TextView rpgdInformation;
+    private Application app;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View result = inflater.inflate(R.layout.fragment_gpdr, container, false);
+        return result;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (Application) requireActivity().getApplication();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
