f712b88b61db355da9d12aff46a81da56adade79
jam7k <jam@uninova.pt>
2021-01-13 14:15:37 +0000

    Added scrollable functionality to OpenSourceLicensesFragment

--- app/src/main/java/pt/uninova/s4h/citizenhub/OpenSourceLicensesFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/OpenSourceLicensesFragment.java
@@ -11,21 +11,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class OpenSourceLicensesFragment extends Fragment {
 
-    private TextView rpgdInformation;
-    private Application app;
+    private TextView openSourceLicensesButton;
+    private ArrayList<String> original_data=new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View result = inflater.inflate(R.layout.fragment_privacy_policy, container, false);
+        final View result = inflater.inflate(R.layout.fragment_open_source_licenses, container, false);
+        TextView openSourceLicensesButton = (TextView) result.findViewById(R.id.text_open_source_licenses);
+
+        Scanner data_in = new Scanner(getResources().openRawResource(R.raw.open_source_licenses));
+        while(data_in.hasNext()){
+            original_data.add(data_in.nextLine());
+        }
+        openSourceLicensesButton.setText("");
+        for(int i=0; i< original_data.size(); i++){
+            openSourceLicensesButton.append(original_data.get(i) +"\n");
+        }
         return result;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        app = (Application) requireActivity().getApplication();
     }
 
     @Override
