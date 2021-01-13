1f2e211d6fac4106a17ddcc4b08d30171254c69e
jam7k <jam@uninova.pt>
2021-01-13 13:36:20 +0000

    Added read from .txt functionality to PrivacyPolicyFragment

--- app/src/main/java/pt/uninova/s4h/citizenhub/PrivacyPolicyFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/PrivacyPolicyFragment.java
@@ -11,26 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PrivacyPolicyFragment extends Fragment {
 
-    private TextView rpgdInformation;
     private Application app;
+    private ArrayList<String> original_data=new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View result = inflater.inflate(R.layout.fragment_privacy_policy, container, false);
+        app = (Application) requireActivity().getApplication();
+        TextView privacyPolicyText = (TextView) result.findViewById(R.id.text_privacy_policy);
+
+        Scanner data_in = new Scanner(getResources().openRawResource(R.raw.privacypolicy));
+        while(data_in.hasNext()){
+            original_data.add(data_in.nextLine());
+        }
+        privacyPolicyText.setText("");
+        for(int i=0; i< original_data.size(); i++){
+            privacyPolicyText.append(original_data.get(i) +"\n");
+        }
         return result;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        app = (Application) requireActivity().getApplication();
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 
     @Override
