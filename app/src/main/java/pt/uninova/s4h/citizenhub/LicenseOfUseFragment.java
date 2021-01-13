ae30fb4174d5cf508fcd34facf73801b9f58e40e
jam7k <jam@uninova.pt>
2021-01-13 13:50:00 +0000

    Added .txt reading functionality to LicenseOfUseFragment

--- app/src/main/java/pt/uninova/s4h/citizenhub/LicenseOfUseFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/LicenseOfUseFragment.java
@@ -11,21 +11,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LicenseOfUseFragment extends Fragment {
 
     private TextView licenseTextView;
     private Application app;
+    private ArrayList<String> original_data=new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View result = inflater.inflate(R.layout.fragment_license_of_use, container, false);
+        app = (Application) requireActivity().getApplication();
+        TextView licenseOfUseButton = (TextView) result.findViewById(R.id.text_license_of_use);
+
+        Scanner data_in = new Scanner(getResources().openRawResource(R.raw.license_of_use));
+        while(data_in.hasNext()){
+            original_data.add(data_in.nextLine());
+        }
+        licenseOfUseButton.setText("");
+        for(int i=0; i< original_data.size(); i++){
+            licenseOfUseButton.append(original_data.get(i) +"\n");
+        }
         return result;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        app = (Application) requireActivity().getApplication();
     }
 
     @Override
