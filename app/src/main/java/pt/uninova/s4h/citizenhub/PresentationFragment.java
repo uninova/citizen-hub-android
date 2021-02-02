24e9c02a762bc0018ff16553d2b39ae6cdfddf2d
jam7k <jam@uninova.pt>
2021-02-02 16:32:05 +0000

    Fixed no context error by moving code to onViewCrated instead of onCreateView

--- app/src/main/java/pt/uninova/s4h/citizenhub/PresentationFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/PresentationFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -20,18 +21,19 @@ public class PresentationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View result = inflater.inflate(R.layout.fragment_presentation, container, false);
-        skipButton = result.findViewById(R.id.button_add_device);
-
-        skipButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Navigation.findNavController(requireView()).navigate(PresentationFragmentDirections.actionPresentationFragmentToAuthenticationFragment());
-            }
-        });
+        skipButton = result.findViewById(R.id.presentation_fragment_skip_button);
 
         return result;
     }
 
-
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(requireView()).navigate(PresentationFragmentDirections.actionPresentationFragmentToAuthenticationFragment());
+            }
+        });
+    }
 }
