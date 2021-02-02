b95026c34b33fb5bcdde6d2c2667fa327ad6ef5e
jam7k <jam@uninova.pt>
2021-02-02 16:31:50 +0000

    Fixed no context error by moving code to onViewCrated instead of onCreateView

--- app/src/main/java/pt/uninova/s4h/citizenhub/EntryPageFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/EntryPageFragment.java
@@ -1,7 +1,11 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -9,19 +13,27 @@ import androidx.navigation.Navigation;
 import java.io.File;
 
 public class EntryPageFragment  extends Fragment {
-    private final File file = new File(requireContext().getFilesDir(),"first.txt");
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.entry_page_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         onEntry();
     }
 
     public boolean firstInteraction(){
+        final File file = new File(requireContext().getFilesDir(),"first.txt");
         return !file.exists();
     }
 
     public void onEntry(){
+
         if(firstInteraction())
         {
             Navigation.findNavController(requireView()).navigate(EntryPageFragmentDirections.actionEntryPageFragmentToPresentationFragment());
