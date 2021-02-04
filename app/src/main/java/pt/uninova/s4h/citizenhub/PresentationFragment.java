15287d9adfae56e77b859d1c5ae22974e244f788
jam7k <jam@uninova.pt>
2021-02-04 19:47:44 +0000

    Created functionality to Write File

--- app/src/main/java/pt/uninova/s4h/citizenhub/PresentationFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/PresentationFragment.java
@@ -11,6 +11,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import java.io.File;
+import java.io.IOException;
+
 public class PresentationFragment extends Fragment {
     private Button skipButton;
 
@@ -19,19 +22,34 @@ public class PresentationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View result = inflater.inflate(R.layout.fragment_presentation, container, false);
         skipButton = result.findViewById(R.id.presentation_fragment_skip_button);
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final File file = new File(requireContext().getFilesDir(), "first.txt");
 
+                    boolean fileCreated = false;
+
+                    try {
+                        fileCreated = file.createNewFile();
+                    }
+                    catch (IOException ioe) {
+                        System.out.println("Error while creating empty file: " + ioe);
+                    }
+
+                    if (fileCreated) {
+                        System.out.println("Created empty file: " + file.getPath());
+                    }
+                    else {
+                        System.out.println("Failed to create empty file: " + file.getPath());
+                    }
+                    Navigation.findNavController(requireView()).navigate(PresentationFragmentDirections.actionPresentationFragmentToAuthenticationFragment());
+            }
+        });
         return result;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        skipButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(requireView()).navigate(PresentationFragmentDirections.actionPresentationFragmentToAuthenticationFragment());
-            }
-        });
     }
-
 }
