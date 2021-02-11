506d0fe83fcd546a3279ffcba4d441a7d0818f62
jam7k <jam@uninova.pt>
2021-02-11 11:43:06 +0000

    Added citizen hub logo's text as a textview for future uses.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/lobby/AuthenticationFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/lobby/AuthenticationFragment.java
@@ -3,10 +3,14 @@ package pt.uninova.s4h.citizenhub.ui.lobby;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -22,11 +26,24 @@ import static care.data4life.sdk.Data4LifeClient.D4L_AUTH;
 
 public class AuthenticationFragment extends Fragment {
     private Button loginButton;
+    private TextView citizenHubLogo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         final View result = inflater.inflate(R.layout.fragment_authentication, container, false);
+        TextView textView = result.findViewById(R.id.citizen_hub_text_logo);
+        Spannable word = new SpannableString("Citizen");
+
+        word.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorS4HDarkBlue)), 0, word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(word);
+        Spannable wordTwo = new SpannableString("HUB");
+
+        wordTwo.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorS4HTurquoise)), 0, wordTwo.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.append(wordTwo);
+
+
         loginButton = result.findViewById(R.id.authentication_fragment_login_button);
         loginButton.setOnClickListener((View v) -> {
             final Intent loginIntent = Data4LifeClient.getInstance().getLoginIntent(requireActivity(), null);
