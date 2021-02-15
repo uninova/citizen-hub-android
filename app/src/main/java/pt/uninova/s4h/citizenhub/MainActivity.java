06040739a01c905e39ff0f8ff736f5ae5ca1a56f
Carlos Lopes <csl@uninova.pt>
2021-02-15 00:49:18 +0000

    Cleaned up activity implementation

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -6,15 +6,12 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
@@ -22,33 +19,26 @@ import pt.uninova.s4h.citizenhub.service.CitizenHubService;
 import pt.uninova.s4h.citizenhub.service.CitizenHubServiceBinder;
 import pt.uninova.s4h.citizenhub.service.CitizenHubServiceBound;
 
-import java.time.LocalDate;
-import java.util.List;
-
-
 public class MainActivity extends AppCompatActivity implements CitizenHubServiceBound {
 
-    boolean mBound = false;
-    private AppBarConfiguration appBarConfiguration;
-    private NavController navController;
-    private CitizenHubServiceBinder citizenHubServiceBinder;
     private CitizenHubService citizenHubService;
-    private ServiceConnection connection = new ServiceConnection() {
+    private ServiceConnection connection;
 
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            CitizenHubServiceBinder binder = (CitizenHubServiceBinder) service;
-            citizenHubService = binder.getService();
-            mBound = true;
-        }
+    private AppBarConfiguration appBarConfiguration;
 
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
+    @Override
+    public CitizenHubService getService() {
+        return citizenHubService;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
         }
-    };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +46,14 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
 
         setContentView(R.layout.activity_main);
 
-        citizenHubServiceBinder = new CitizenHubServiceBinder(citizenHubService);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavController navController = ((NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
         NavigationView navView = findViewById(R.id.nav_view);
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
         appBarConfiguration =
                 new AppBarConfiguration.Builder(R.id.summary_fragment, R.id.report_master_fragment, R.id.device_list_fragment, R.id.logout_fragment, R.id.about_fragment)
@@ -75,52 +65,55 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Intent intent = new Intent(this, CitizenHubService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
-        unbindService(connection);
-        mBound = false;
+
+        stopService();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startService();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
+        NavController navController = ((NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-            fragment.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+    private void startService() {
+        this.connection = new ServiceConnection() {
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        List<Fragment> fragments = getSupportFragmentManager().getFragments();
-        if (fragments != null) {
-            for (Fragment fragment : fragments) {
-                fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            @Override
+            public void onServiceConnected(ComponentName className, IBinder service) {
+                citizenHubService = ((CitizenHubServiceBinder) service).getService();
             }
-        }
+
+            @Override
+            public void onServiceDisconnected(ComponentName arg0) {
+                MainActivity.this.connection = null;
+            }
+        };
+
+        final Intent intent = new Intent(this, CitizenHubService.class);
+
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
-    @Override
-    public CitizenHubService getService() {
-        return citizenHubService;
+    private void stopService() {
+        unbindService(connection);
     }
 }
