8ff7e6b449803b3f8287e9ae0a8f8fd695b45e12
jam <jam@uninova.pt>
2021-03-08 15:50:40 +0000

    Added auto-scroll feature to viewpager.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/lobby/SlideActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/lobby/SlideActivity.java
@@ -3,9 +3,13 @@ package pt.uninova.s4h.citizenhub.ui.lobby;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import pt.uninova.s4h.citizenhub.R;
 
@@ -13,6 +17,10 @@ public class SlideActivity extends AppCompatActivity {
 
     public static ViewPager viewPager;
     SlideViewPagerAdapter adapter;
+    final long DELAY_MS = 500;
+    final long PERIOD_MS = 2000;
+    int currentPage = 0;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,25 @@ public class SlideActivity extends AppCompatActivity {
             editor.putBoolean("slide", true);
             editor.apply();
         }
+
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == 4) {
+                    handler.removeCallbacks(this);
+                } else {
+                    viewPager.setCurrentItem(currentPage++, true);
+                }
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_MS, PERIOD_MS);
 
 
     }
