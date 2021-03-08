61a47af949bb649d1dac7b07de586781459fe60b
jam <jam@uninova.pt>
2021-03-08 14:35:49 +0000

    Added feature for automatic "open e-mail app" with pre-filled fields.

--- app/src/main/java/pt/uninova/s4h/citizenhub/AboutFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/AboutFragment.java
@@ -1,11 +1,13 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -30,6 +32,18 @@ public class AboutFragment extends Fragment {
 
 
         Button privacyPolicyButton = result.findViewById(R.id.about_privacy_policy_button);
+        TextView emailTextView = result.findViewById(R.id.text_about_contact);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"helpdesk@smart4health.eu"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "CitizenHub: Issue Report");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                requireContext().startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            }
+        });
 
         privacyPolicyButton.setOnClickListener(
                 v -> Navigation.findNavController(requireView()).navigate(AboutFragmentDirections.actionAboutFragmentToPrivacyPolicyFragment()));
