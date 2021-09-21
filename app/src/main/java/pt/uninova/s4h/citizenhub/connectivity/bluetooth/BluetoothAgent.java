b04992164a9d0f07b6e8a4357c0e7102590a8107
Carlos Lopes <csl@uninova.pt>
2021-09-21 15:53:55 +0100

    Adapted BluetoothAgent

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/BluetoothAgent.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/BluetoothAgent.java
@@ -1,26 +1,19 @@
 package pt.uninova.s4h.citizenhub.connectivity.bluetooth;
 
-import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import pt.uninova.s4h.citizenhub.connectivity.AbstractAgent;
-import pt.uninova.s4h.citizenhub.connectivity.AgentState;
-import pt.uninova.s4h.citizenhub.connectivity.Protocol;
 import pt.uninova.s4h.citizenhub.persistence.MeasurementKind;
 
 public abstract class BluetoothAgent extends AbstractAgent {
 
     final private BluetoothConnection connection;
 
-    protected BluetoothAgent(UUID id, Map<UUID, Protocol> protocolMap, BluetoothConnection connection) {
-        super(id, protocolMap);
+    protected BluetoothAgent(UUID id, Set<UUID> supportedProtocolsIds, Set<MeasurementKind> supportedMeasurements, BluetoothConnection connection) {
+        super(id, supportedProtocolsIds, supportedMeasurements);
 
         this.connection = connection;
-    }
-
-    private BluetoothAgent() {
-        super(null, null);
-        this.connection = null;
     }
 
     protected BluetoothConnection getConnection() {
