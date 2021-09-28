9cdb17a2aa376c6dcec98e4fac9a1cd2500f6911
Carlos Lopes <csl@uninova.pt>
2021-09-28 16:00:26 +0100

    Add mechanism for delayed removal

--- app/src/main/java/pt/uninova/util/messaging/Dispatcher.java
+++ app/src/main/java/pt/uninova/util/messaging/Dispatcher.java
@@ -2,29 +2,66 @@ package pt.uninova.util.messaging;
 
 import java.io.Closeable;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Dispatcher<T> implements Closeable {
 
     final private Set<Observer<T>> observerSet;
+    final private Set<Observer<T>> delayedRemoval;
+
+    private boolean dispatching;
 
     public Dispatcher() {
         observerSet = new HashSet<>();
+        delayedRemoval = new HashSet<>();
+
+        dispatching = false;
+    }
+
+    public void addObserver(Observer<T> observer) {
+        synchronized (observerSet) {
+            this.observerSet.add(observer);
+        }
     }
 
     @Override
     public void close() {
-        observerSet.clear();
-    }
-
-    public void dispatch(T message) {
-        for (Observer<T> i : observerSet) {
-            i.onChanged(message);
+        synchronized (observerSet) {
+            observerSet.clear();
         }
     }
 
-    public Set<Observer<T>> getObservers() {
-        return observerSet;
+    public synchronized void dispatch(T message) {
+        synchronized (observerSet) {
+            dispatching = true;
+        }
+
+        for (Observer<T> observer : observerSet) {
+            observer.observe(message);
+        }
+
+        final Iterator<Observer<T>> i = delayedRemoval.iterator();
+
+        while (i.hasNext()) {
+            final Observer<T> observer = i.next();
+
+            observerSet.remove(observer);
+            i.remove();
+        }
+
+        synchronized (observerSet) {
+            dispatching = false;
+        }
     }
 
+    public void removeObserver(Observer<T> observer) {
+        synchronized (observerSet) {
+            if (dispatching) {
+                delayedRemoval.add(observer);
+            } else {
+                this.observerSet.remove(observer);
+            }
+        }
+    }
 }
