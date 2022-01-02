ed8238872e26b7b756d84e1dbe46af06bb23006b
Carlos Lopes <csl@uninova.pt>
2022-01-02 10:18:16 +0000

    Add account management to view model

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsViewModel.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.preference.PreferenceManager;
 
+import care.data4life.sdk.Data4LifeClient;
+
 public class AccountsViewModel extends AndroidViewModel {
 
     private final SharedPreferences preferences;
@@ -17,12 +19,43 @@ public class AccountsViewModel extends AndroidViewModel {
         preferences = PreferenceManager.getDefaultSharedPreferences(application.getApplicationContext());
     }
 
+    public void disableSmartBearAccount() {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.remove("accounts.smartbear.id");
+        editor.putBoolean("accounts.smartbear.enabled", false);
+
+        editor.apply();
+    }
+
+    public void enableSmartBearAccount(int id) {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putBoolean("accounts.smartbear.enabled", true);
+        editor.putInt("accounts.smartbear.id", id);
+
+        editor.apply();
+
+    }
+
+    public int getSmartBearId() {
+        return preferences.getInt("accounts.smartbear.id", -1);
+    }
+
     public boolean hasSmart4HealthAccount() {
         return preferences.getBoolean("accounts.smart4health.enabled", true);
     }
 
     public boolean hasSmartBearAccount() {
         return preferences.getBoolean("accounts.smartbear.enabled", false);
+    }
+
+    public void setSmartBearId(int value) {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putInt("accounts.smartbear.id", value);
+
+        editor.apply();
     }
 
 }
