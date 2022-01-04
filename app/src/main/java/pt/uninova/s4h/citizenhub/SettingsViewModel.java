f621a09dad8239e770fb94ab41f869e229fe17d4
jam7k <jam@uninova.pt>
2022-01-04 13:18:49 +0000

    Added "Preference" building block from AndroidX Preference library, suggested in the documentation ( https://developer.android.com/guide/topics/ui/settings ).

--- app/src/main/java/pt/uninova/s4h/citizenhub/SettingsViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/SettingsViewModel.java
@@ -1,12 +1,21 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.preference.PreferenceManager;
 
 public class SettingsViewModel extends AndroidViewModel {
+
+    private final SharedPreferences preferences;
+
+
     public SettingsViewModel(@NonNull Application application) {
+
         super(application);
+        preferences = PreferenceManager.getDefaultSharedPreferences(application.getApplicationContext());
+
     }
 }
