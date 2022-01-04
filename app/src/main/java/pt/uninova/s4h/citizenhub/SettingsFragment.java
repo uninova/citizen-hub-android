8525baa2dff32a7a59cc678886d191cce7b533eb
jam7k <jam@uninova.pt>
2022-01-04 17:02:16 +0000

    Created custom TimePicker to handle range of hours (Work Hours).

--- app/src/main/java/pt/uninova/s4h/citizenhub/SettingsFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/SettingsFragment.java
@@ -1,9 +1,15 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -13,5 +19,35 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     }
 
+
+
+    @Override
+    public void onDisplayPreferenceDialog(Preference preference)
+    {
+        DialogFragment dialogFragment = null;
+        if (preference instanceof TimePreference)
+        {
+            dialogFragment = new TimePreferenceDialogFragmentCompat();
+            Bundle bundle = new Bundle(1);
+            bundle.putString("key", preference.getKey());
+            dialogFragment.setArguments(bundle);
+
+        }
+
+        if (dialogFragment != null)
+        {
+            dialogFragment.setTargetFragment(this, 0);
+            dialogFragment.show(this.getFragmentManager(), "android.support.v7.preference.PreferenceFragment.DIALOG");
+        }
+        else
+        {
+            super.onDisplayPreferenceDialog(preference);
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
 }
