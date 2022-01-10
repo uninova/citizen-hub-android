c8df41ee3bc54ef6b898fd7e890ee1784447ec70
jam7k <jam@uninova.pt>
2022-01-10 12:26:20 +0000

    Undo this commit

---
+++ app/src/main/java/pt/uninova/util/WorkTimeRangeConverter.java
@@ -0,0 +1,15 @@
+package pt.uninova.util;
+
+public class WorkTimeRangeConverter {
+
+    //get shared preferences values
+    //get current time & compare (when and where?)
+
+
+    //if () return isWorking
+    //usar em todos os inserts(?)
+
+
+    //reports -> 2 queries, 2 layouts
+    //summary -> igual (?)
+}
