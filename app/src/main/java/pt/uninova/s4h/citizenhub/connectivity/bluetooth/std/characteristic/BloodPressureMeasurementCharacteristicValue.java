e5007c3f32ccd8f4181cc862efa0d4b680e48906
Carlos Lopes <csl@uninova.pt>
2022-02-10 17:32:11 +0000

    Add support for blood pressure measurements

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/std/characteristic/BloodPressureMeasurementCharacteristicValue.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/std/characteristic/BloodPressureMeasurementCharacteristicValue.java
@@ -3,6 +3,11 @@ package pt.uninova.s4h.citizenhub.connectivity.bluetooth.std.characteristic;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import pt.uninova.s4h.citizenhub.data.BloodPressureMeasurement;
+import pt.uninova.s4h.citizenhub.data.BloodPressureValue;
+import pt.uninova.s4h.citizenhub.data.Measurement;
+import pt.uninova.s4h.citizenhub.data.PulseRateMeasurement;
+
 public class BloodPressureMeasurementCharacteristicValue {
 
     public static final int UNIT_PRESSURE_MILLIMETRE_OF_MERCURY = 0;
@@ -36,23 +41,60 @@ public class BloodPressureMeasurementCharacteristicValue {
         if (hasTimeStamp()) {
             timeStampDate = Transcoding.date(buffer[offset++], buffer[offset++], buffer[offset++], buffer[offset++]);
             timeStampTime = Transcoding.time(buffer[offset++], buffer[offset++], buffer[offset++]);
+        } else {
+            timeStampDate = null;
+            timeStampTime = null;
         }
 
         if (hasPulseRate()) {
             pulseRate = Transcoding.sfloat(buffer[offset++], buffer[offset++]);
+        } else {
+            pulseRate = -1;
         }
 
         if (hasUserId()) {
             userId = Transcoding.uint8(buffer[offset++]);
+        } else {
+            userId = -1;
         }
 
         if (hasMeasurementStatus()) {
-
+            measurementStatus = 0;
+        } else {
+            measurementStatus = 0;
         }
+    }
+
+    public double getDiastolicValue() {
+        return diastolicValue;
+    }
+
+    public double getMeanArterialPressureValue() {
+        return meanArterialPressureValue;
     }
 
     public int getPressureUnit() {
         return (flags & 0x1);
+    }
+
+    public double getPulseRate() {
+        return pulseRate;
+    }
+
+    public double getSystolicValue() {
+        return systolicValue;
+    }
+
+    public LocalDate getTimeStampDate() {
+        return timeStampDate;
+    }
+
+    public LocalTime getTimeStampTime() {
+        return timeStampTime;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public boolean hasTimeStamp() {
@@ -69,5 +111,17 @@ public class BloodPressureMeasurementCharacteristicValue {
 
     public boolean hasMeasurementStatus() {
         return (flags & 0x10) != 0;
+    }
+
+    public Measurement<?>[] toMeasurements() {
+        Measurement<?>[] measurements = new Measurement<?>[1 + (hasPulseRate() ? 1 : 0)];
+
+        measurements[0] = new BloodPressureMeasurement(new BloodPressureValue(systolicValue, diastolicValue, meanArterialPressureValue));
+
+        if (hasPulseRate()) {
+            measurements[1] = new PulseRateMeasurement(pulseRate);
+        }
+
+        return measurements;
     }
 }
---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/data/PulseRateMeasurement.java
@@ -0,0 +1,8 @@
+package pt.uninova.s4h.citizenhub.data;
+
+public class PulseRateMeasurement extends Measurement<Double> {
+
+    public PulseRateMeasurement(double value) {
+        super(PULSE_RATE, value);
+    }
+}
