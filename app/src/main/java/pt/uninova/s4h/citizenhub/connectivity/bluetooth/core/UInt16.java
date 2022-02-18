4d8f606553236845f9a0813ef34d4a6e690d3916
Carlos Lopes <csl@uninova.pt>
2022-02-18 16:08:55 +0000

    Add heart rate implementation

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/core/UInt16.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/core/UInt16.java
@@ -1,8 +1,8 @@
 package pt.uninova.s4h.citizenhub.connectivity.bluetooth.core;
 
-public class UInt16 implements ByteSerializable {
+import java.util.Objects;
 
-    private final byte b1, b2;
+public class UInt16 implements ByteSerializable {
 
     public static UInt16 of(byte b1, byte b2) {
         return new UInt16(b1, b2);
@@ -12,9 +12,24 @@ public class UInt16 implements ByteSerializable {
         return new UInt16((byte) (val & 0xff), (byte) (val >>> 8 & 0xff));
     }
 
+    private final byte b1, b2;
+
     private UInt16(byte b1, byte b2) {
         this.b1 = b1;
         this.b2 = b2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UInt16 uInt16 = (UInt16) o;
+        return b1 == uInt16.b1 && b2 == uInt16.b2;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(b1, b2);
     }
 
     @Override
@@ -23,7 +38,7 @@ public class UInt16 implements ByteSerializable {
     }
 
     public int toInt() {
-        return (b2 & 0xff) << 8 | b1 & 0xff;
+        return b1 | b2 << 8;
     }
 
     @Override
