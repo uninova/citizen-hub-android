5c736ae96608e7b116d34a32600b9048c043db5f
jam7k <jam@uninova.pt>
2022-03-16 17:19:11 +0000

    Implemented Class extension of Sample to handle the relation of primary/foreign keys.

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/SampleWithMeasurements.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/SampleWithMeasurements.java
@@ -3,18 +3,17 @@ package pt.uninova.s4h.citizenhub.persistence;
 import androidx.room.Relation;
 
 import java.time.Instant;
-import java.util.List;
 
 public class SampleWithMeasurements extends Sample {
 
     @Relation(parentColumn = "id", entityColumn = "id", entity = BloodPressureMeasurement.class)
-    private List<BloodPressureMeasurement> bloodPressureMeasurement;
+    private BloodPressureMeasurement bloodPressureMeasurement;
 
     public SampleWithMeasurements(Integer id, Instant timestamp, String device_address) {
         super(id, timestamp, device_address);
     }
 
-    public List<BloodPressureMeasurement> get(){
+    public BloodPressureMeasurement getBloodPressureMeasurement(){
         return this.bloodPressureMeasurement;
     }
 
