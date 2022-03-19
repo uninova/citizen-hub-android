06a735261f064ba90e1efc818247750547bbeeb1
Carlos Lopes <csl@uninova.pt>
2022-03-19 16:37:12 +0000

    Remove unused imports

--- app/src/main/java/pt/uninova/s4h/citizenhub/SummaryViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/SummaryViewModel.java
@@ -6,7 +6,6 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 import pt.uninova.s4h.citizenhub.persistence.LumbarExtensionTraining;
