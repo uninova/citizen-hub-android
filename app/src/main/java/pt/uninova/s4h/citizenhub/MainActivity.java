9a44747e48380831e8a23fe527d270a5cd297559
Carlos Lopes <csl@uninova.pt>
2022-03-19 16:34:04 +0000

    Removed deprecated permissions code and service initialization

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -1,16 +1,10 @@
 package pt.uninova.s4h.citizenhub;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,31 +12,9 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
-import pt.uninova.s4h.citizenhub.service.CitizenHubService;
-import pt.uninova.s4h.citizenhub.service.CitizenHubServiceBinder;
-import pt.uninova.s4h.citizenhub.service.CitizenHubServiceBound;
-
-public class MainActivity extends AppCompatActivity implements CitizenHubServiceBound {
-
-    private CitizenHubService citizenHubService;
-    private ServiceConnection connection;
+public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-
-    @Override
-    public CitizenHubService getService() {
-        return citizenHubService;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-            fragment.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,30 +38,6 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        stopService();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-            fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        startService();
     }
 
     @Override
@@ -97,25 +45,6 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
         NavController navController = ((NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
 
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
-    }
-
-    private void startService() {
-        this.connection = new ServiceConnection() {
-
-            @Override
-            public void onServiceConnected(ComponentName className, IBinder service) {
-                citizenHubService = ((CitizenHubServiceBinder) service).getService();
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName arg0) {
-                MainActivity.this.connection = null;
-            }
-        };
-
-        final Intent intent = new Intent(this, CitizenHubService.class);
-
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
@@ -131,7 +60,4 @@ public class MainActivity extends AppCompatActivity implements CitizenHubService
         }
     }
 
-    private void stopService() {
-        unbindService(connection);
-    }
 }
