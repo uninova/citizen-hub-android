d64c2e416b77c1146bdaed2bec0855dbea73e266
Carlos Lopes <csl@uninova.pt>
2022-03-22 00:14:53 +0000

    Introduced the Bluetooth fragment

--- app/src/main/java/pt/uninova/s4h/citizenhub/DeviceSearchFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceSearchFragment.java
@@ -1,18 +1,13 @@
 package pt.uninova.s4h.citizenhub;
 
-import android.Manifest;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -24,7 +19,7 @@ import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothScanner;
 import pt.uninova.s4h.citizenhub.persistence.ConnectionKind;
 import pt.uninova.s4h.citizenhub.ui.devices.DeviceViewModel;
 
-public class DeviceSearchFragment extends Fragment {
+public class DeviceSearchFragment extends BluetoothFragment {
 
     private DeviceListAdapter adapter;
     private DeviceViewModel model;
@@ -53,26 +48,34 @@ public class DeviceSearchFragment extends Fragment {
 
         model = new ViewModelProvider(requireActivity()).get(DeviceViewModel.class);
 
-        ActivityResultLauncher<String[]> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
-            boolean granted = true;
+    }
 
-            for (boolean i : result.values()) {
-                granted = granted && i;
+    @Override
+    protected void onBluetoothAllowed() {
+        scanner = new BluetoothScanner((BluetoothManager) requireActivity().getSystemService(Context.BLUETOOTH_SERVICE));
+
+        scanner.start((address, name) -> {
+            final Device device = new Device(address, name == null ? address : name, ConnectionKind.BLUETOOTH);
+
+            if (model.getAgentOrchestrator().getValue().getAgent(device) == null) {
+                adapter.addItem(new DeviceListItem(device, R.drawable.ic_devices_unpaired));
             }
-
-            if (granted)
-                startScan();
         });
+    }
 
-        final String[] permissions;
+    @Override
+    protected void onBluetoothDenied() {
+        Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment());
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permissions = new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION};
-        } else {
-            permissions = new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION};
-        }
+    @Override
+    protected void onBluetoothDisabled() {
+        Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment());
+    }
 
-        requestPermissionLauncher.launch(permissions);
+    @Override
+    protected void onBluetoothUnsupported() {
+        Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment());
     }
 
     @Override
@@ -85,6 +88,16 @@ public class DeviceSearchFragment extends Fragment {
     }
 
     @Override
+    protected void onLocationDisabled() {
+        Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment());
+    }
+
+    @Override
+    protected void onLocationUnsupported() {
+        Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment());
+    }
+
+    @Override
     public void onStop() {
         super.onStop();
 
@@ -92,67 +105,5 @@ public class DeviceSearchFragment extends Fragment {
             scanner.stop();
             scanner = null;
         }
-    }
-
-    /*
-    private void enableLocation() {
-        if (hasStartedEnableLocationActivity) {
-            new AlertDialog.Builder(getContext())
-                    .setMessage(R.string.fragment_device_search_location_warning_title)
-                    .setPositiveButton(R.string.fragment_device_search_location_open_settings_button, (paramDialogInterface, paramInt) -> {
-                        requireContext().startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                        paramDialogInterface.dismiss();
-                    })
-                    .setNegativeButton(R.string.fragment_device_search_cancel_option, (dialog, which) -> Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment()))
-                    .show();
-        } else {
-            hasStartedEnableLocationActivity = true;
-            new AlertDialog.Builder(getContext())
-                    .setMessage(R.string.fragment_device_search_location_not_enabled_title)
-                    .setPositiveButton(R.string.fragment_device_search_location_open_settings_button, (paramDialogInterface, paramInt) -> {
-                        requireContext().startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                        paramDialogInterface.dismiss();
-                    })
-                    .setNegativeButton(R.string.fragment_device_search_cancel_option, (paramDialogInterface, paramInt) -> checkPermissions())
-                    .show();
-        }
-    }
-
-    private void enableBluetooth() {
-        if (hasStartedEnableBluetoothActivity) {
-            new AlertDialog.Builder(getContext())
-                    .setMessage(R.string.fragment_device_search_bluetooth_warning_title)
-                    .setPositiveButton(R.string.fragment_device_search_bluetooth_open_settings_button, (paramDialogInterface, paramInt) -> {
-                        requireContext().startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-                        paramDialogInterface.dismiss();
-                    })
-                    .setNegativeButton(R.string.fragment_device_search_cancel_option, (dialog, which) -> Navigation.findNavController(requireView()).navigate(DeviceSearchFragmentDirections.actionDeviceSearchFragmentToDeviceListFragment()))
-                    .show();
-
-        } else {
-            hasStartedEnableBluetoothActivity = true;
-            new AlertDialog.Builder(getContext())
-                    .setMessage(R.string.fragment_device_search_bluetooth_not_enabled_title)
-                    .setPositiveButton(R.string.fragment_device_search_bluetooth_open_settings_button, (paramDialogInterface, paramInt) -> {
-                        requireContext().startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-                        paramDialogInterface.dismiss();
-                    })
-                    .setNegativeButton(R.string.fragment_device_search_cancel_option, (paramDialogInterface, paramInt) -> checkPermissions())
-                    .show();
-        }
-    }
-
-     */
-
-    private void startScan() {
-        scanner = new BluetoothScanner((BluetoothManager) requireActivity().getSystemService(Context.BLUETOOTH_SERVICE));
-
-        scanner.start((address, name) -> {
-            final Device device = new Device(address, name == null ? address : name, ConnectionKind.BLUETOOTH);
-
-            if (model.getAgentOrchestrator().getValue().getAgent(device) == null) {
-                adapter.addItem(new DeviceListItem(device, R.drawable.ic_devices_unpaired));
-            }
-        });
     }
 }
