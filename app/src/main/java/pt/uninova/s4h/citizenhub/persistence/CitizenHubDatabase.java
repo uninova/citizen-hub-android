f95eefa25bed014b7c7c7c50f1849dd04b10f2e5
Carlos Lopes <csl@uninova.pt>
2022-03-29 11:46:30 +0100

    Reset exportSchema

--- app/src/main/java/pt/uninova/s4h/citizenhub/persistence/CitizenHubDatabase.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/persistence/CitizenHubDatabase.java
@@ -2,39 +2,27 @@ package pt.uninova.s4h.citizenhub.persistence;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.Migration;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {DeviceRecord.class, Measurement.class, Feature.class, LumbarExtensionTraining.class, SmartBearUploadDateRecord.class}, version = 36, exportSchema = false)
+@Database(entities = {DeviceRecord.class, Measurement.class, Feature.class, LumbarExtensionTraining.class, SmartBearUploadDateRecord.class}, version = 36)
 public abstract class CitizenHubDatabase extends RoomDatabase {
 
     private static final int NUMBER_OF_THREADS = 4;
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
     private static volatile CitizenHubDatabase INSTANCE;
 
-
-    static final Migration MIGRATION_32_33 = new Migration(32, 33) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE measurement "
-                    + " ADD COLUMN is_working INTEGER");
-        }
-
-    };
-
     public static CitizenHubDatabase getInstance(final Context context) {
         if (INSTANCE == null) {
             synchronized (CitizenHubDatabase.class) {
                 if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), CitizenHubDatabase.class, "citizen_hub_database").fallbackToDestructiveMigration()
-                            .addMigrations(MIGRATION_32_33).build();
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), CitizenHubDatabase.class, "citizen_hub_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
                 }
             }
         }
@@ -48,11 +36,11 @@ public abstract class CitizenHubDatabase extends RoomDatabase {
 
     public abstract DeviceDao deviceDao();
 
-    public abstract MeasurementDao measurementDao();
-
     public abstract FeatureDao featureDao();
 
     public abstract LumbarExtensionTrainingDao lumbarExtensionTrainingDao();
+
+    public abstract MeasurementDao measurementDao();
 
     public abstract SmartBearUploadDateDao smartBearUploadDateDao();
 }
