edfa138f428cdd9a50ad4ca3add8906dd5fe7fab
Fábio Lopes <fl@uninova.pt>
2022-05-03 22:50:25 +0100

    Navigation for both methods of connection.

--- app/src/main/java/pt/uninova/s4h/citizenhub/MethodConnectionAdapter.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MethodConnectionAdapter.java
@@ -7,9 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-
+import androidx.navigation.Navigation;
 import java.util.ArrayList;
 
 public class MethodConnectionAdapter extends ArrayAdapter<String> {
@@ -26,16 +25,22 @@ public class MethodConnectionAdapter extends ArrayAdapter<String> {
 
         ImageView methodImage = convertView.findViewById(R.id.image_method);
         TextView methodName = convertView.findViewById(R.id.text_view_title_method);
-        // Populate the data into the template view using the data object
+
         if(name.equals("Bluetooth")) {
             methodImage.setImageResource(R.drawable.ic_bluetooth);
             methodName.setText("Bluetooth");
+            View finalConvertView = convertView;
+            convertView.setOnClickListener(view -> Navigation.findNavController(finalConvertView)
+                    .navigate(MethodConnectionFragmentDirections.actionDeviceConnectionMethodFragmentToDeviceSearchFragment()));
         }
         else if (name.equals("WearOS")){
             methodImage.setImageResource(R.drawable.ic_wearos);
             methodName.setText("WearOS");
+            View finalConvertView = convertView;
+            convertView.setOnClickListener(view -> Navigation.findNavController(finalConvertView)
+                    .navigate(MethodConnectionFragmentDirections.actionDeviceConnectionMethodFragmentToDeviceSearchWearosFragment()));
         }
-        // Return the completed view to render on screen
+
         return convertView;
     }
 }
--- app/src/main/java/pt/uninova/s4h/citizenhub/MethodConnectionFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MethodConnectionFragment.java
@@ -4,13 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,26 +20,9 @@ public class MethodConnectionFragment extends Fragment {
         final View result = inflater.inflate(R.layout.fragment_device_connection_method, container, false);
 
         ListView listConnectionMethods = result.findViewById(R.id.listViewConnectionMethods);
-
         ArrayList<String> connectionMethods = new ArrayList<>(connectionMethodsAvailable);
-
         adapter = new MethodConnectionAdapter(getContext(),connectionMethods);
         listConnectionMethods.setAdapter(adapter);
-
-        /*listConnectionMethods.setOnItemClickListener((adapterView, view, i, l) -> {
-            if (connectionMethods.get(i).equals("WearOS"))
-                Navigation.findNavController(requireView()).navigate(MethodConnectionFragmentDirections.actionDeviceConnectionMethodFragmentToDeviceSearchWearosFragment());
-            if (connectionMethods.get(i).equals("Bluetooth"))
-                Navigation.findNavController(requireView()).navigate(MethodConnectionFragmentDirections.actionDeviceConnectionMethodFragmentToDeviceSearchFragment());
-        });*/
-
-        listConnectionMethods.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
-
         return result;
     }
 
