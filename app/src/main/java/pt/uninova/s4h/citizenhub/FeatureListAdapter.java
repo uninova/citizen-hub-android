681386d124c89256393c8b9e803a69c204f58eaa
Carlos Lopes <csl@uninova.pt>
2022-05-04 17:58:31 +0100

    Refresh adapter design

--- app/src/main/java/pt/uninova/s4h/citizenhub/FeatureListAdapter.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/FeatureListAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.widget.SwitchCompat;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class FeatureListAdapter extends BaseAdapter {
@@ -17,47 +19,34 @@ class FeatureListAdapter extends BaseAdapter {
     private static LayoutInflater inflater = null;
     private final List<FeatureListItem> data;
     CompoundButton.OnCheckedChangeListener switchListener;
-    private FeatureListAdapter.OnCheckedChangeListener listener;
+
 
     public FeatureListAdapter(Context context, List<FeatureListItem> data) {
         this.data = data;
+        Collections.sort(this.data, Comparator.comparing(FeatureListItem::getLabel));
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public static String capitalizeString(String string) {
-        String[] arr = string.split(" ");
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(Character.toUpperCase(arr[i].charAt(0)))
-                    .append(arr[i].substring(1)).append(" ");
-        }
-        return sb.toString().trim();
-    }
-
-    public void setOnItemClickListener(FeatureListAdapter.OnCheckedChangeListener listen) {
-        listener = listen;
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         View vi = convertView;
         if (vi == null)
             vi = inflater.inflate(R.layout.list_item_feature, null);
+
         SwitchCompat nameSwitch = vi.findViewById(R.id.switchFeature);
-        switchListener = new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                data.get(position).setActive(isChecked);
-                notifyDataSetChanged();
-            }
+
+        switchListener = (buttonView, isChecked) -> {
+            data.get(position).setActive(isChecked);
+            notifyDataSetChanged();
         };
+
         nameSwitch.setOnCheckedChangeListener(null);
         nameSwitch.setChecked(data.get(position).isActive());
         nameSwitch.setOnCheckedChangeListener(switchListener);
 
         TextView text = vi.findViewById(R.id.textFeature);
-        String lastString = capitalizeString(data.get(position).getMeasurementKind().toString().toLowerCase().replace("_", " "));
-        text.setText(lastString);
+        text.setText(data.get(position).getLabel());
 
         return vi;
     }
@@ -68,6 +57,7 @@ class FeatureListAdapter extends BaseAdapter {
         }
         if (results != null) {
             data.addAll(results);
+            Collections.sort(data, Comparator.comparing(FeatureListItem::getLabel));
             this.notifyDataSetChanged();
         }
     }
@@ -85,10 +75,6 @@ class FeatureListAdapter extends BaseAdapter {
     @Override
     public long getItemId(int position) {
         return position;
-    }
-
-    public interface OnCheckedChangeListener {
-        void onCheckedChange(int position);
     }
 
 }
