697bb086f4af389805ac7f072efa34b5e43634c3
jam7k <jam@uninova.pt>
2022-05-18 12:04:52 +0100

    Added item to appBarConfiguration

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -1,7 +1,13 @@
 package pt.uninova.s4h.citizenhub;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -29,16 +35,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = ((NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
         NavigationView navView = findViewById(R.id.nav_view);
         navView.setItemIconTintList(null);
+
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
         appBarConfiguration =
-                new AppBarConfiguration.Builder(R.id.summary_fragment, R.id.report_master_fragment, R.id.lumbar_extension_training_fragment, R.id.device_list_fragment, R.id.settings_fragment, R.id.accounts_fragment, R.id.about_fragment)
+                new AppBarConfiguration.Builder(R.id.summary_fragment, R.id.report_master_fragment, R.id.lumbar_extension_training_fragment, R.id.device_list_fragment, R.id.settings_fragment,R.id.smart4health_fragment, R.id.accounts_fragment, R.id.about_fragment)
                         .setOpenableLayout(drawerLayout)
                         .build();
 
+
+
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -46,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
-
     @Override
     public void onBackPressed() {
         NavController navController = ((NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
