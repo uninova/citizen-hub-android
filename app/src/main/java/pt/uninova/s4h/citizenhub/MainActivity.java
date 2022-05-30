0384cddfebcf381c3d62c78be1e22223e2264a4e
Carlos Lopes <csl@uninova.pt>
2022-05-30 14:28:50 +0100

    Move s4h check to onResume

--- app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/MainActivity.java
@@ -18,12 +18,10 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         setContentView(R.layout.activity_main);
 
@@ -41,14 +39,21 @@ public class MainActivity extends AppCompatActivity {
                 new AppBarConfiguration.Builder(R.id.summary_fragment, R.id.report_master_fragment, R.id.lumbar_extension_training_fragment, R.id.device_list_fragment, R.id.settings_fragment, R.id.smart4health_fragment, R.id.accounts_fragment, R.id.about_fragment)
                         .setOpenableLayout(drawerLayout)
                         .build();
-        Menu nav_Menu = navView.getMenu();
-        nav_Menu.findItem(R.id.smart4health_fragment).setVisible(preferences.getBoolean("accounts.smart4health.enabled", true));
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final NavigationView navView = findViewById(R.id.nav_view);
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final Menu nav_Menu = navView.getMenu();
+
+        nav_Menu.findItem(R.id.smart4health_fragment).setVisible(preferences.getBoolean("accounts.smart4health.enabled", false));
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
