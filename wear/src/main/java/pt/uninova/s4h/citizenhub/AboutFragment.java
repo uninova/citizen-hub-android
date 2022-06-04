1037153b9efbe69e8f54cfd8fe3b18e1c683c6ae
Fábio Lopes <fl@uninova.pt>
2022-06-04 01:35:17 +0100

    Removes unecessary casts.

--- wear/src/main/java/pt/uninova/s4h/citizenhub/AboutFragment.java
+++ wear/src/main/java/pt/uninova/s4h/citizenhub/AboutFragment.java
@@ -12,7 +12,7 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return (ViewGroup) inflater.inflate(
+        return inflater.inflate(
                 R.layout.fragment_about, container, false);
     }
 }
