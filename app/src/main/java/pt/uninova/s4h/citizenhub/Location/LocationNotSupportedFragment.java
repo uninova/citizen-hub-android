a53ef89c1389e581d2eeffc1e4408d9b03e28852
jam7k <jam@uninova.pt>
2022-06-06 08:57:51 +0100

    Added LocationNotSupportedFragment.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/Location/LocationNotSupportedFragment.java
@@ -0,0 +1,29 @@
+package pt.uninova.s4h.citizenhub.Location;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import pt.uninova.s4h.citizenhub.R;
+
+public class LocationNotSupportedFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View result = inflater.inflate(R.layout.fragment_location_not_supported, container, false);
+        return result;
+
+    }
+
+}
