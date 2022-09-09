cbd93dfefb661c63fe43b4965d56b18c688c0749
Carlos Lopes <csl@uninova.pt>
2022-09-09 01:16:01 +0100

    Use new algorithm to reconnect Mi Band 2

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/miband2/MiBand2HeartRateProtocol.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/miband2/MiBand2HeartRateProtocol.java
@@ -5,8 +5,11 @@ import android.os.Looper;
 
 import java.util.UUID;
 
+import pt.uninova.s4h.citizenhub.BuildConfig;
+import pt.uninova.s4h.citizenhub.connectivity.Agent;
 import pt.uninova.s4h.citizenhub.connectivity.AgentOrchestrator;
 import pt.uninova.s4h.citizenhub.connectivity.Protocol;
+import pt.uninova.s4h.citizenhub.connectivity.StateChangedMessage;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BaseCharacteristicListener;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BaseDescriptorListener;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothAgent;
@@ -17,6 +20,7 @@ import pt.uninova.s4h.citizenhub.connectivity.bluetooth.DescriptorListener;
 import pt.uninova.s4h.citizenhub.data.HeartRateMeasurement;
 import pt.uninova.s4h.citizenhub.data.Sample;
 import pt.uninova.s4h.citizenhub.util.messaging.Dispatcher;
+import pt.uninova.s4h.citizenhub.util.messaging.Observer;
 
 public class MiBand2HeartRateProtocol extends BluetoothMeasuringProtocol {
 
@@ -25,7 +29,6 @@ public class MiBand2HeartRateProtocol extends BluetoothMeasuringProtocol {
     private final DescriptorListener heartRateMeasurementClientCharacteristicConfigurationListener = new BaseDescriptorListener(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT, BluetoothAgent.UUID_DESCRIPTOR_CLIENT_CHARACTERISTIC_CONFIGURATION) {
         @Override
         public void onWrite(byte[] value) {
-            getConnection().removeDescriptorListener(this);
             getConnection().writeCharacteristic(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_CONTROL_POINT, new byte[]{0x15, 0x02, 0x00});
         }
     };
@@ -46,9 +49,9 @@ public class MiBand2HeartRateProtocol extends BluetoothMeasuringProtocol {
                 h.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        getConnection().writeCharacteristic(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_CONTROL_POINT, new byte[]{0x16});
-
                         if (getState() == Protocol.STATE_ENABLED) {
+                            getConnection().writeCharacteristic(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_CONTROL_POINT, new byte[]{0x16});
+
                             h.postDelayed(this, 10000);
                         }
                     }
@@ -62,8 +65,30 @@ public class MiBand2HeartRateProtocol extends BluetoothMeasuringProtocol {
         @Override
         public void onChange(byte[] value) {
             final pt.uninova.s4h.citizenhub.connectivity.bluetooth.core.HeartRateMeasurement val = new pt.uninova.s4h.citizenhub.connectivity.bluetooth.core.HeartRateMeasurement(value);
+            int i = val.getValue().toInt();
 
-            getSampleDispatcher().dispatch(new Sample(getAgent().getSource(), new HeartRateMeasurement(val.getValue().toInt())));
+            if (i != 0) {
+                getSampleDispatcher().dispatch(new Sample(getAgent().getSource(), new HeartRateMeasurement(i)));
+            }
+        }
+    };
+
+    private final Observer<StateChangedMessage<Integer, ? extends Agent>> agentStateChangeObserver = value -> {
+        if (BuildConfig.DEBUG)
+            System.out.println("MiBand2HeartRateProtocol.agentStateChangeObserver " + value.getNewState() + " " + value.getOldState());
+
+        if (value.getNewState() == Agent.AGENT_STATE_ENABLED) {
+            getConnection().addDescriptorListener(heartRateMeasurementClientCharacteristicConfigurationListener);
+            getConnection().addCharacteristicListener(heartRateControlPointListener);
+            getConnection().addCharacteristicListener(heartRateMeasurementListener);
+
+            getConnection().enableNotifications(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
+        } else {
+            setState(Protocol.STATE_SUSPENDED);
+            getConnection().removeCharacteristicListener(heartRateControlPointListener);
+            getConnection().removeCharacteristicListener(heartRateMeasurementListener);
+            getConnection().removeDescriptorListener(heartRateMeasurementClientCharacteristicConfigurationListener);
+            getConnection().disableNotifications(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
         }
     };
 
@@ -73,19 +98,26 @@ public class MiBand2HeartRateProtocol extends BluetoothMeasuringProtocol {
 
     @Override
     public void disable() {
+        getConnection().removeCharacteristicListener(heartRateControlPointListener);
         getConnection().removeCharacteristicListener(heartRateMeasurementListener);
+        getConnection().removeDescriptorListener(heartRateMeasurementClientCharacteristicConfigurationListener);
         getConnection().disableNotifications(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
+
+        getAgent().removeStateObserver(agentStateChangeObserver);
 
         super.disable();
     }
 
     @Override
     public void enable() {
-        getConnection().addDescriptorListener(heartRateMeasurementClientCharacteristicConfigurationListener);
-        getConnection().addCharacteristicListener(heartRateControlPointListener);
+        getAgent().addStateObserver(agentStateChangeObserver);
 
-        getConnection().addCharacteristicListener(heartRateMeasurementListener);
+        if (getAgent().getState() == Agent.AGENT_STATE_ENABLED) {
+            getConnection().addDescriptorListener(heartRateMeasurementClientCharacteristicConfigurationListener);
+            getConnection().addCharacteristicListener(heartRateControlPointListener);
+            getConnection().addCharacteristicListener(heartRateMeasurementListener);
 
-        getConnection().enableNotifications(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
+            getConnection().enableNotifications(BluetoothAgent.UUID_SERVICE_HEART_RATE, BluetoothAgent.UUID_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
+        }
     }
 }
