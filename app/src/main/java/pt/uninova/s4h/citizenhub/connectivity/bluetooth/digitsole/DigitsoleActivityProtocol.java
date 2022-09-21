a8b718e7c48dfc37fbd006fed35817c69c5d3b6b
Fábio Lopes <fl@uninova.pt>
2022-09-21 23:00:30 +0100

    Reconnecting from out-of-reach and restarting app.

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/digitsole/DigitsoleActivityProtocol.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/digitsole/DigitsoleActivityProtocol.java
@@ -8,11 +8,9 @@ import java.util.UUID;
 
 import pt.uninova.s4h.citizenhub.connectivity.AgentOrchestrator;
 import pt.uninova.s4h.citizenhub.connectivity.Protocol;
-import pt.uninova.s4h.citizenhub.connectivity.StateChangedMessage;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BaseCharacteristicListener;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothAgent;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothConnection;
-import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothConnectionState;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.BluetoothMeasuringProtocol;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.CharacteristicListener;
 import pt.uninova.s4h.citizenhub.data.CaloriesMeasurement;
@@ -20,49 +18,46 @@ import pt.uninova.s4h.citizenhub.data.DistanceMeasurement;
 import pt.uninova.s4h.citizenhub.data.Sample;
 import pt.uninova.s4h.citizenhub.data.StepsMeasurement;
 import pt.uninova.s4h.citizenhub.util.messaging.Dispatcher;
-import pt.uninova.s4h.citizenhub.util.messaging.Observer;
 
 public class DigitsoleActivityProtocol extends BluetoothMeasuringProtocol {
 
     public static final UUID UUID_SERVICE_DATA = UUID.fromString("99ddcdab-a80c-4f94-be5d-c66b9fba40cf");
-    private static final UUID UUID_CHARACTERISTIC_ACTIVITYLOG = UUID.fromString("99dd0106-a80c-4f94-be5d-c66b9fba40cf");
-    private static final UUID UUID_CHARACTERISTIC_COLLECTINGSTATE = UUID.fromString("99dd0014-a80c-4f94-be5d-c66b9fba40cf");
+    private static final UUID UUID_CHARACTERISTIC_ACTIVITY_LOG = UUID.fromString("99dd0106-a80c-4f94-be5d-c66b9fba40cf");
+    private static final UUID UUID_CHARACTERISTIC_COLLECTING_STATE = UUID.fromString("99dd0014-a80c-4f94-be5d-c66b9fba40cf");
 
     public static final UUID ID = AgentOrchestrator.namespaceGenerator().getUUID("bluetooth.digitsole.activity");
 
     long lastTime = 0;
-    int miliForTimer = 120000; //2 minutes
+    int milliForTimer = 120000;
 
-    private final CharacteristicListener activationListener = new BaseCharacteristicListener(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTINGSTATE) {
+    private final CharacteristicListener activationListener = new BaseCharacteristicListener(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTING_STATE) {
         private boolean once0x02 = false;
 
         @Override
         public void onWrite(byte[] value) {
-            System.out.println("RAN ACTIVATIONLISTENER");
             switch (value[0]) {
                 case 0x00:
-                    getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTINGSTATE, new byte[]{0x02});
+                    getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTING_STATE, new byte[]{0x02});
                     break;
                 case 0x02:
                     if (once0x02) {
-                        getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTINGSTATE, new byte[]{0x01});
+                        getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTING_STATE, new byte[]{0x01});
                     } else {
                         once0x02 = true;
-                        getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTINGSTATE, new byte[]{0x02});
+                        getConnection().writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTING_STATE, new byte[]{0x02});
                     }
                     break;
                 case 0x01:
                     once0x02 = false;
-                    setState(Protocol.STATE_ENABLED);
+
                     break;
             }
         }
     };
 
-    private final CharacteristicListener dataListener = new BaseCharacteristicListener(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_ACTIVITYLOG) {
+    private final CharacteristicListener dataListener = new BaseCharacteristicListener(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_ACTIVITY_LOG) {
         @Override
         public void onChange(byte[] value) {
-            System.out.println("RAN DATALISTENER");
             double steps = value[4] & 0xff;
 
             DecimalFormat f = new DecimalFormat("##.00");
@@ -80,58 +75,30 @@ public class DigitsoleActivityProtocol extends BluetoothMeasuringProtocol {
         }
     };
 
-    private final Observer<StateChangedMessage<BluetoothConnectionState, BluetoothConnection>> reconnectionListener = value -> {
-        if (value.getNewState() == BluetoothConnectionState.READY) {
-            //enable();
-            System.out.println("GOT HERE1 " + getConnection().getState());
-        }
-        else if (value.getNewState() == BluetoothConnectionState.CONNECTED){
-            System.out.println("GOT HERE2 " + getConnection().getState());
-        }
-        else if (value.getNewState() == BluetoothConnectionState.DISCONNECTED)
-        {
-            System.out.println("GOT HERE3" + getConnection().getState());
-            disable(); //TODO confirm need for this
-        }
-    };
-
     public DigitsoleActivityProtocol(BluetoothConnection connection, Dispatcher<Sample> dispatcher, BluetoothAgent agent) {
         super(ID, connection, dispatcher, agent);
     }
 
     @Override
     public void disable() {
-        System.out.println("RUNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN DISABLE");
-        final BluetoothConnection connection = getConnection();
-        connection.disableNotifications(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_ACTIVITYLOG);
-        connection.removeCharacteristicListener(dataListener);
-        connection.removeCharacteristicListener(activationListener);
-        super.disable();
+        setState(Protocol.STATE_DISABLED);
     }
 
     @Override
     public void enable() {
-        System.out.println("RUNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ENABLE");
         long currentTime = System.currentTimeMillis();
         runTimer();
-        //Last segment is recent, restarting timer...
-        if ((currentTime - lastTime) < miliForTimer) {
-            System.out.println("Last segment is recent...");
-        }
-        //Last segment is old or first time connecting, enabling...
-        else{
+        if ((currentTime - lastTime) > milliForTimer) {
             final BluetoothConnection connection = getConnection();
-            //System.out.println(connection.getState());
-            System.out.println("Doing enable");
-            connection.addConnectionStateChangeListener(reconnectionListener);
             connection.addCharacteristicListener(activationListener);
             connection.addCharacteristicListener(dataListener);
-            connection.enableNotifications(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_ACTIVITYLOG, true);
-            connection.writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTINGSTATE, new byte[]{0x00});
+            connection.enableNotifications(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_ACTIVITY_LOG, true);
+            connection.writeCharacteristic(UUID_SERVICE_DATA, UUID_CHARACTERISTIC_COLLECTING_STATE, new byte[]{0x00});
+            setState(Protocol.STATE_ENABLED);
         }
     }
 
     private void runTimer() {
-        new Handler(Looper.getMainLooper()).postDelayed(this::enable, miliForTimer);
+        new Handler(Looper.getMainLooper()).postDelayed(this::enable, milliForTimer);
     }
 }
