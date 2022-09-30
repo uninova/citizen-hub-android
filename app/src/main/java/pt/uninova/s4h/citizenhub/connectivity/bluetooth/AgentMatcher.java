191249d730bcb34595b4955d924f4f41bb0c8406
jam <jam@uninova.pt>
2022-09-30 15:50:51 +0100

    Added AgentMatcher interface.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/AgentMatcher.java
@@ -0,0 +1,12 @@
+package pt.uninova.s4h.citizenhub.connectivity.bluetooth;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AgentMatcher {
+
+    boolean doesMatch(BluetoothConnection connection);
+    Class<?> getAgentClass();
+    List<UUID> getAgentServices();
+
+}
