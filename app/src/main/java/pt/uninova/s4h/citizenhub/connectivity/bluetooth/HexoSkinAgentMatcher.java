0f8cdce42cda24c42911e33ed96436425179da16
jam7k <jam@uninova.pt>
2022-10-03 00:11:12 +0100

    Fixed wrong HexoSkin services UUID's in agentServices.

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/HexoSkinAgentMatcher.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/HexoSkinAgentMatcher.java
@@ -5,16 +5,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import pt.uninova.s4h.citizenhub.connectivity.bluetooth.hexoskin.HexoSkinAccelerometerProtocol;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.hexoskin.HexoSkinAgent;
-import pt.uninova.s4h.citizenhub.connectivity.bluetooth.miband2.MiBand2Agent;
+import pt.uninova.s4h.citizenhub.connectivity.bluetooth.hexoskin.HexoSkinHeartRateProtocol;
+import pt.uninova.s4h.citizenhub.connectivity.bluetooth.hexoskin.HexoSkinRespirationProtocol;
 
 public class HexoSkinAgentMatcher implements AgentMatcher {
 
     private static final List<UUID> agentServices;
+
     static {
         agentServices = Collections.unmodifiableList(Arrays.asList(
-                HexoSkinAgent.UUID_SERVICE_HEART_RATE,
-                HexoSkinAgent.UUID_SERVICE_BLOOD_PRESSURE));
+                HexoSkinAccelerometerProtocol.ACCELEROMETER_SERVICE_UUID,
+                HexoSkinRespirationProtocol.RESPIRATION_SERVICE_UUID,
+                HexoSkinHeartRateProtocol.UUID_SERVICE_HEART_RATE,
+                HexoSkinRespirationProtocol.RESPIRATION_SERVICE_UUID));
     }
 
     @Override
