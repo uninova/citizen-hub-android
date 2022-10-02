3c85f73704da2e965fa8d1356b29b5d128d1bcde
jam7k <jam@uninova.pt>
2022-10-03 00:06:23 +0100

    Added KbzPostureAgentMatcher.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/KbzPostureAgentMatcher.java
@@ -0,0 +1,44 @@
+package pt.uninova.s4h.citizenhub.connectivity.bluetooth;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import pt.uninova.s4h.citizenhub.connectivity.bluetooth.kbzposture.KbzBodyProtocol;
+import pt.uninova.s4h.citizenhub.connectivity.bluetooth.kbzposture.KbzPostureAgent;
+
+public class KbzPostureAgentMatcher implements AgentMatcher {
+
+    private static final List<UUID> agentServices;
+
+    static {
+        agentServices = Collections.singletonList(
+                KbzBodyProtocol.KBZ_SERVICE);
+    }
+
+    @Override
+    public boolean doesMatch(BluetoothConnection connection) {
+        boolean doesMatch = true;
+        for (UUID service : agentServices
+        ) {
+            if (connection.hasService(service)) {
+                System.out.println("Agent " + getAgentClass() + "Has service: " + service);
+            } else {
+                System.out.println("Agent " + getAgentClass() + "DOESN'T HAVE service: " + service);
+                doesMatch = false;
+            }
+            System.out.println("Kbz Match? " + doesMatch);
+        }
+        return doesMatch;
+    }
+
+    @Override
+    public Class<?> getAgentClass() {
+        return KbzPostureAgent.class;
+    }
+
+    @Override
+    public List<UUID> getAgentServices() {
+        return agentServices;
+    }
+}
