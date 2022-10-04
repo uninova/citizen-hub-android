47663c362296219e9382a6367b9297c0531d9042
jam <jam@uninova.pt>
2022-10-04 12:11:28 +0100

    Removed extends class from DeviceUnsupportedFragment & fixed its consequences.

--- app/src/main/java/pt/uninova/s4h/citizenhub/DeviceUnsupportedFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceUnsupportedFragment.java
@@ -4,36 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import pt.uninova.s4h.citizenhub.data.Device;
 import pt.uninova.s4h.citizenhub.ui.devices.DeviceViewModel;
 
-public class DeviceUnsupportedFragment extends DeviceConfigurationFragment {
+public class DeviceUnsupportedFragment extends Fragment {
     private DeviceViewModel model;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         model = new ViewModelProvider(requireActivity()).get(DeviceViewModel.class);
+
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_device_unsupported, container, false);
-        model.getSelectedDevice();
-        nameDevice = view.findViewById(R.id.textUnsupportedDevice);
-        addressDevice = view.findViewById(R.id.textUnsupportedDeviceAddress);
-        setDeviceText();
+        final Device device = model.getSelectedDevice().getValue();
+
+        TextView nameDevice = view.findViewById(R.id.textUnsupportedDevice);
+        TextView deviceAddress = view.findViewById(R.id.textUnsupportedDeviceAddress);
+        nameDevice.setText(getString(R.string.fragment_configuration_text_view_name, device.getName()));
+        deviceAddress.setText(getString(R.string.fragment_configuration_text_view_address, device.getAddress()));
 
         return view;
     }
-
-
-    protected void setDeviceText() {
-        final Device device = model.getSelectedDevice().getValue();
-
-        nameDevice.setText(getString(R.string.fragment_configuration_text_view_name, device.getName()));
-        addressDevice.setText(getString(R.string.fragment_configuration_text_view_address, device.getAddress()));
-    }
-    }
+}
