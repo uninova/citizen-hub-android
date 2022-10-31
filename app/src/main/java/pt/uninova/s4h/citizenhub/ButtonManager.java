c43da08607d4c83ce266ecab044f5e73e1e68035
jam <jam@uninova.pt>
2022-10-31 15:01:17 +0000

    Added Menu to the new class as it will be the desired return.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ButtonManager.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ButtonManager.java
@@ -1,16 +1,21 @@
 package pt.uninova.s4h.citizenhub;
 
+import android.view.Menu;
 import android.view.View;
 
 public class ButtonManager {
 
     private int resourceId;
     private View.OnClickListener onClickListener;
+    private Menu menu;
 
-    public ButtonManager(int resourceId, View.OnClickListener onClickListener) {
+    public ButtonManager(int resourceId, View.OnClickListener onClickListener, Menu menu) {
         this.resourceId = resourceId;
         this.onClickListener = onClickListener;
+        this.menu = menu;
     }
+
+
 
     public int getResourceId() {
         return resourceId;
@@ -26,5 +31,13 @@ public class ButtonManager {
 
     public void setOnClickListener(View.OnClickListener onClickListener) {
         this.onClickListener = onClickListener;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
     }
 }
