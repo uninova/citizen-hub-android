dd817d9ad8c8b87f00aebb3d44f40a9c9d3314ee
jam <jam@uninova.pt>
2022-10-31 14:59:35 +0000

    Added class to handle dynamic buttons in AdvancedConfigurations

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceConfigurationButtonManager.java
@@ -0,0 +1,30 @@
+package pt.uninova.s4h.citizenhub;
+
+import android.view.View;
+
+public class DeviceConfigurationButtonManager {
+
+    private int resourceId;
+    private View.OnClickListener onClickListener;
+
+    public DeviceConfigurationButtonManager(int resourceId, View.OnClickListener onClickListener) {
+        this.resourceId = resourceId;
+        this.onClickListener = onClickListener;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public View.OnClickListener getOnClickListener() {
+        return onClickListener;
+    }
+
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+}
