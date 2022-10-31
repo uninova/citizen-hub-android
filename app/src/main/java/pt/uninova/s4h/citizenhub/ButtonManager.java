04e01188eb150afe9e3bd524d936533206b90808
jam <jam@uninova.pt>
2022-10-31 15:06:52 +0000

    Implemented Add&Remove methods. Changed default OnClickListener to MenuItem.OnMenuItemClickListener.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ButtonManager.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ButtonManager.java
@@ -1,21 +1,30 @@
 package pt.uninova.s4h.citizenhub;
 
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ButtonManager {
 
     private int resourceId;
-    private View.OnClickListener onClickListener;
+    private MenuItem.OnMenuItemClickListener onClickListener;
     private Menu menu;
 
-    public ButtonManager(int resourceId, View.OnClickListener onClickListener, Menu menu) {
+    public ButtonManager(int resourceId, MenuItem.OnMenuItemClickListener onClickListener) {
         this.resourceId = resourceId;
         this.onClickListener = onClickListener;
-        this.menu = menu;
     }
 
+    public Menu addButton(Menu menu){
+        menu.add(resourceId);
+        menu.getItem(resourceId).setOnMenuItemClickListener(onClickListener);
+        return menu;
+    }
 
+    public Menu removeButton(Menu menu){
+        menu.removeItem(resourceId);
+        return menu;
+    }
 
     public int getResourceId() {
         return resourceId;
@@ -25,11 +34,11 @@ public class ButtonManager {
         this.resourceId = resourceId;
     }
 
-    public View.OnClickListener getOnClickListener() {
+    public MenuItem.OnMenuItemClickListener getOnClickListener() {
         return onClickListener;
     }
 
-    public void setOnClickListener(View.OnClickListener onClickListener) {
+    public void setOnClickListener(MenuItem.OnMenuItemClickListener onClickListener) {
         this.onClickListener = onClickListener;
     }
 
