e296ae2e028aeb77dc1d9f498d3281e565d007fa
jam <jam@uninova.pt>
2022-11-11 16:13:05 +0000

    Added needed fragments to implement the new layouts.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationFeaturesFragment.java
@@ -0,0 +1,7 @@
+package pt.uninova.s4h.citizenhub.ui.devices;
+
+import androidx.fragment.app.Fragment;
+
+public class DeviceConfigurationFeaturesFragment extends Fragment {
+
+}
---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationStreamsFragment.java
@@ -0,0 +1,6 @@
+package pt.uninova.s4h.citizenhub.ui.devices;
+
+import androidx.fragment.app.Fragment;
+
+public class DeviceConfigurationStreamsFragment extends Fragment {
+}
