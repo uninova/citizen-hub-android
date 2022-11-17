d64cac7151f0c840c9f443d1c90576f21b6d3d76
jam <jam@uninova.pt>
2022-11-17 16:03:09 +0000

    Added boilerplate code for Device Unique Identifier.

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationUniqueIdentifierFragment.java
@@ -0,0 +1,22 @@
+package pt.uninova.s4h.citizenhub.ui.devices;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import pt.uninova.s4h.citizenhub.R;
+
+public class DeviceConfigurationUniqueIdentifierFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_device_configuration_unique_identifier, container, false);
+        return view;
+    }
+}
