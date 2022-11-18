4e14fe2d2ae9ed6b898068c3e2b8de836e7bd6d1
jam <jam@uninova.pt>
2022-11-18 10:56:15 +0000

    Designed fragment_device_configuration_unique_identifier_item.xml.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationUniqueIdentifierFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationUniqueIdentifierFragment.java
@@ -16,7 +16,7 @@ public class DeviceConfigurationUniqueIdentifierFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_device_configuration_unique_identifier, container, false);
+        final View view = inflater.inflate(R.layout.fragment_device_configuration_unique_identifier_item, container, false);
         return view;
     }
 }
