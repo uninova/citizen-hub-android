94d1dca0fff54f887d0c4c321bd0467fce24e8a1
jam <jam@uninova.pt>
2022-11-18 12:27:04 +0000

    Added EditText logic.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationUdiSetterFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/devices/DeviceConfigurationUdiSetterFragment.java
@@ -1,10 +1,13 @@
 package pt.uninova.s4h.citizenhub.ui.devices;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,14 +18,21 @@ import pt.uninova.s4h.citizenhub.R;
 
 public class DeviceConfigurationUdiSetterFragment extends Fragment {
 
-    private Button setUdi;
-    private Button clearUdi;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_device_configuration_unique_identifier_setter, container, false);
-        setUdi = view.findViewById(R.id.udi_set_button);
+        Button setUdi = view.findViewById(R.id.udi_set_button);
+        Button clearUdi = view.findViewById(R.id.udi_clear_button);
+        EditText udiText = view.findViewById(R.id.fragment_device_configuration_udi_setter_textview);
+
+        udiText.setOnClickListener(view1 -> {
+            udiText.requestFocus();
+
+            InputMethodManager inputMethodManager = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showSoftInput(view1, InputMethodManager.SHOW_IMPLICIT);
+        });
+
         setUdi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
