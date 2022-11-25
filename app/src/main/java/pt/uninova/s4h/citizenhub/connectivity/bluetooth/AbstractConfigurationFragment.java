9f95ab14c36be270ce959b43f233195cbe2e6513
jam <jam@uninova.pt>
2022-11-25 10:18:50 +0000

    Created more abstraction by assigning control of listviews & query to AbstractConfigurationFragment.

--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/AbstractConfigurationFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/AbstractConfigurationFragment.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import pt.uninova.s4h.citizenhub.connectivity.Agent;
+import pt.uninova.s4h.citizenhub.localization.MeasurementKindLocalization;
+import pt.uninova.s4h.citizenhub.ui.devices.FeatureListAdapter;
+import pt.uninova.s4h.citizenhub.ui.devices.FeatureListItem;
 
 public class AbstractConfigurationFragment extends Fragment {
 
     private Agent agent;
-
+    public MeasurementKindLocalization measurementKindLocalization;
     public AbstractConfigurationFragment(Agent agent) {
         this.agent = agent;
     }
@@ -22,6 +30,8 @@ public class AbstractConfigurationFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        measurementKindLocalization = new MeasurementKindLocalization(requireContext());
+
     }
 
     @Nullable
@@ -32,6 +42,40 @@ public class AbstractConfigurationFragment extends Fragment {
 
     public void enableView(View view, boolean state) {
         view.setEnabled(state);
+    }
+
+    protected void loadSupportedFeatures(ListView listView) {
+        FeatureListAdapter adapter;
+        if (agent != null) {
+            adapter = new FeatureListAdapter(requireActivity(), getSupportedFeatures(), agent.getState() == 1, agent);
+
+        } else {
+            adapter = new FeatureListAdapter(requireActivity(), getSupportedFeatures(), agent);
+
+        }
+        listView.setAdapter(adapter);
+        adapter.updateResults(getSupportedFeatures());
+    }
+
+    protected List<FeatureListItem> getSupportedFeatures() {
+        final List<FeatureListItem> featureListItems = new LinkedList<>();
+        if (agent != null) {
+
+            if (agent.getState() != 1 && agent.getEnabledMeasurements() != null) {
+
+                for (int i : agent.getSupportedMeasurements()) {
+                    featureListItems.add(new FeatureListItem(i, measurementKindLocalization.localize(i), agent.getEnabledMeasurements().contains(i)));
+                }
+            } else {
+                final Set<Integer> measurementKindSet = agent.getEnabledMeasurements();
+
+                for (int i : agent.getSupportedMeasurements()) {
+                    featureListItems.add(new FeatureListItem(i, measurementKindLocalization.localize(i), measurementKindSet.contains(i)));
+                }
+            }
+        }
+
+        return featureListItems;
     }
 
 }
--- app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/uprightgo2/UprightGo2StreamsFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/connectivity/bluetooth/uprightgo2/UprightGo2StreamsFragment.java
@@ -9,28 +9,20 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-
 import pt.uninova.s4h.citizenhub.R;
 import pt.uninova.s4h.citizenhub.connectivity.Agent;
 import pt.uninova.s4h.citizenhub.connectivity.StateChangedMessage;
 import pt.uninova.s4h.citizenhub.connectivity.bluetooth.AbstractConfigurationFragment;
 import pt.uninova.s4h.citizenhub.localization.MeasurementKindLocalization;
-import pt.uninova.s4h.citizenhub.ui.devices.FeatureListAdapter;
-import pt.uninova.s4h.citizenhub.ui.devices.FeatureListItem;
 import pt.uninova.s4h.citizenhub.util.messaging.Observer;
 
 public class UprightGo2StreamsFragment extends AbstractConfigurationFragment {
     private final Agent agent;
     private ListView streamsListView;
-    private MeasurementKindLocalization measurementKindLocalization;
 
     private final Observer<StateChangedMessage<Integer, ? extends Agent>> agentStateObserver = value -> {
         streamsListView.deferNotifyDataSetChanged();
-        requireActivity().runOnUiThread(this::loadSupportedFeatures);
-
+        requireActivity().runOnUiThread(() -> loadSupportedFeatures(streamsListView));
     };
 
     public UprightGo2StreamsFragment(Agent agent) {
@@ -49,43 +41,11 @@ public class UprightGo2StreamsFragment extends AbstractConfigurationFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_device_streams_listview, container, false);
         streamsListView = view.findViewById(R.id.streamsListView);
-        loadSupportedFeatures();
+        loadSupportedFeatures(streamsListView);
 
         return view;
     }
 
-    protected void loadSupportedFeatures() {
-        FeatureListAdapter adapter;
-        if (agent != null) {
-            adapter = new FeatureListAdapter(requireActivity(), getSupportedFeatures(), agent.getState() == 1, agent);
 
-        } else {
-            adapter = new FeatureListAdapter(requireActivity(), getSupportedFeatures(), agent);
-
-        }
-        streamsListView.setAdapter(adapter);
-        adapter.updateResults(getSupportedFeatures());
-    }
-
-    protected List<FeatureListItem> getSupportedFeatures() {
-        final List<FeatureListItem> featureListItems = new LinkedList<>();
-        if (agent != null) {
-
-            if (agent.getState() != 1 && agent.getEnabledMeasurements() != null) {
-
-                for (int i : agent.getSupportedMeasurements()) {
-                    featureListItems.add(new FeatureListItem(i, measurementKindLocalization.localize(i), agent.getEnabledMeasurements().contains(i)));
-                }
-            } else {
-                final Set<Integer> measurementKindSet = agent.getEnabledMeasurements();
-
-                for (int i : agent.getSupportedMeasurements()) {
-                    featureListItems.add(new FeatureListItem(i, measurementKindLocalization.localize(i), measurementKindSet.contains(i)));
-                }
-            }
-        }
-
-        return featureListItems;
-    }
 
 }
