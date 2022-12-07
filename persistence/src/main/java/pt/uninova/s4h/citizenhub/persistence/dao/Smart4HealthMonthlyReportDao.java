6f47f069625b1ae364f9e361a50e15956c6d6a8e
João Oliveira <j.oliveira@uninova.pt>
2022-12-07 16:51:13 +0000

    Queries

--- persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/dao/Smart4HealthMonthlyReportDao.java
+++ persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/dao/Smart4HealthMonthlyReportDao.java
@@ -3,7 +3,12 @@ package pt.uninova.s4h.citizenhub.persistence.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.TypeConverters;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import pt.uninova.s4h.citizenhub.persistence.conversion.EpochTypeConverter;
 import pt.uninova.s4h.citizenhub.persistence.entity.Smart4HealthMonthlyReportRecord;
 
 @Dao
@@ -17,5 +22,13 @@ public interface Smart4HealthMonthlyReportDao {
 
     @Query("INSERT OR REPLACE INTO smart4health_monthly_report (year, month, pdf) VALUES (:year, :month, :value);")
     long insertOrReplacePdf(Integer year, Integer month, Boolean value);
+
+    @Query("SELECT * FROM smart4health_monthly_report ORDER BY year DESC, month DESC LIMIT 1;")
+    @TypeConverters(EpochTypeConverter.class)
+    Smart4HealthMonthlyReportRecord selectLastMonthUploaded();
+
+    @Query("SELECT DISTINCT (timestamp / 86400000) * 86400000 FROM sample EXCEPT SELECT date FROM smart4health_daily_report;")
+    @TypeConverters(EpochTypeConverter.class)
+    List<LocalDate> selectMonthsWithValues();
 
 }
--- persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/dao/Smart4HealthWeeklyReportDao.java
+++ persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/dao/Smart4HealthWeeklyReportDao.java
@@ -3,7 +3,12 @@ package pt.uninova.s4h.citizenhub.persistence.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.TypeConverters;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import pt.uninova.s4h.citizenhub.persistence.conversion.EpochTypeConverter;
 import pt.uninova.s4h.citizenhub.persistence.entity.Smart4HealthWeeklyReportRecord;
 
 @Dao
@@ -17,5 +22,12 @@ public interface Smart4HealthWeeklyReportDao {
 
     @Query("INSERT OR REPLACE INTO smart4health_weekly_report (year, week, pdf) VALUES (:year, :week, :value);")
     long insertOrReplacePdf(Integer year, Integer week, Boolean value);
-    
+
+    @Query("SELECT * FROM smart4health_weekly_report ORDER BY year DESC, week DESC LIMIT 1;")
+    @TypeConverters(EpochTypeConverter.class)
+    Smart4HealthWeeklyReportRecord selectLastWeekUploaded();
+
+    @Query("SELECT DISTINCT (timestamp / 86400000) * 86400000 FROM sample EXCEPT SELECT date FROM smart4health_daily_report;")
+    @TypeConverters(EpochTypeConverter.class)
+    List<LocalDate> selectWeeksWithValues();
 }
