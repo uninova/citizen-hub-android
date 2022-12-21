b097e95ba00379e5d63e37c7f4d1285436b39d6f
jam <jam@uninova.pt>
2022-12-21 12:19:05 +0000

    Applied menu changes to SmartBearAccountFragment.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/SmartBearAccountFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/SmartBearAccountFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -23,30 +26,33 @@ public class SmartBearAccountFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.smart_bear_account_fragment, container, false);
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.smart_bear_account_fragment, menu);
+            }
 
-        setHasOptionsMenu(true);
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.smart_bear_account_fragment_menu_advanced) {
+                    Navigation.findNavController(requireView()).navigate(SmartBearAccountFragmentDirections.actionSmartBearAccountFragmentToAdvancedSmartBearAccountGateFragment());
+                    return true;
+                } else if (menuItem.getItemId() == R.id.smart_bear_account_fragment_menu_log_out) {
+                    final AccountsViewModel viewModel = new ViewModelProvider(requireActivity()).get(AccountsViewModel.class);
+                    viewModel.disableSmartBearAccount();
+
+                    WorkOrchestrator workOrchestrator = new WorkOrchestrator(WorkManager.getInstance(requireContext()));
+                    workOrchestrator.cancelSmartBearUploader();
+
+                    Navigation.findNavController(requireView()).navigate(SmartBearAccountFragmentDirections.actionSmartBearAccountFragmentToAccountsFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return view;
-    }
-
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.smart_bear_account_fragment, menu);
-
-        menu.findItem(R.id.smart_bear_account_fragment_menu_advanced).setOnMenuItemClickListener(item -> {
-            Navigation.findNavController(requireView()).navigate(SmartBearAccountFragmentDirections.actionSmartBearAccountFragmentToAdvancedSmartBearAccountGateFragment());
-            return true;
-        });
-
-        menu.findItem(R.id.smart_bear_account_fragment_menu_log_out).setOnMenuItemClickListener(item -> {
-            final AccountsViewModel viewModel = new ViewModelProvider(requireActivity()).get(AccountsViewModel.class);
-            viewModel.disableSmartBearAccount();
-
-            WorkOrchestrator workOrchestrator = new WorkOrchestrator(WorkManager.getInstance(requireContext()));
-            workOrchestrator.cancelSmartBearUploader();
-
-            Navigation.findNavController(requireView()).navigate(SmartBearAccountFragmentDirections.actionSmartBearAccountFragmentToAccountsFragment());
-            return true;
-        });
     }
 }
