459a8f6fdd91c1207cc773cfd939e309044a4b21
jam <jam@uninova.pt>
2022-12-21 14:08:05 +0000

    Removed DeviceListFragment's options menu that were just for testing purposes.

--- app/src/main/java/pt/uninova/s4h/citizenhub/DeviceListFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/DeviceListFragment.java
@@ -5,17 +5,11 @@ import static java.util.Objects.requireNonNull;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.core.view.MenuHost;
-import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -73,18 +67,6 @@ public class DeviceListFragment extends Fragment {
         final View result = inflater.inflate(R.layout.fragment_device_list, container, false);
 
         model = new ViewModelProvider(requireActivity()).get(DeviceViewModel.class);
-        MenuHost menuHost = requireActivity();
-        menuHost.addMenuProvider(new MenuProvider() {
-            @Override
-            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
-                menuInflater.inflate(R.menu.fragment_device_list, menu);
-            }
-
-            @Override
-            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
-                return false;
-            }
-        });
 
         Button searchDevices = result.findViewById(R.id.searchButton);
 
